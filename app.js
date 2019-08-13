fd853ea602375d9dd71ad162ae110da0576f3416
Nick Hurlburt <nick@techmatters.org>
2019-08-12 18:37:22 -0700

    Add POST endpoint that just parrots back JSON

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ const ContactModel = require('./models/contact.js')
 const sequelize = new Sequelize('postgres://hrm@localhost:5432/hrmdb');
 const Contact = ContactModel(sequelize, Sequelize);
 
+app.use(express.json())
+
 // run with node app.js and hit curl localhost:3000/
 app.get('/', function (req, res) {
   Contact.findAll().then(contacts => {
   	res.json(contacts);
   })
+});
+
+// example: curl -XPOST -H'Content-Type: application/json' localhost:3000/contacts -d'{"hi": 2}'
+app.post('/contacts', function(req, res) {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
