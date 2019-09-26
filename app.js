87e542c26d44c69f2fb9721c31005d7412278988
Nick Hurlburt <nick@techmatters.org>
2019-09-26 09:58:02 -0700

    Temporarily remove database connectivity

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-// const port = 8080
-const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('postgres://hrm@localhost:5432/hrmdb');
-const Contact = require('./models/contact.js')(sequelize, Sequelize);
-const AgeBracket = require('./models/agebracket.js')(sequelize, Sequelize);
-const Subcategory = require('./models/subcategory.js')(sequelize, Sequelize);
+// Sequelize init
+// const Sequelize = require('sequelize')
+// const sequelize = new Sequelize('postgres://hrm@localhost:5432/hrmdb');
+// const Contact = require('./models/contact.js')(sequelize, Sequelize);
+// const AgeBracket = require('./models/agebracket.js')(sequelize, Sequelize);
+// const Subcategory = require('./models/subcategory.js')(sequelize, Sequelize);
 
 app.use(express.json())
 app.use(cors())
-sequelize.sync().then(() => console.log("Sequelize synced"));
+// sequelize.sync().then(() => console.log("Sequelize synced"));
 
 app.options('/contacts', cors())
 
@@ -27,30 +27,33 @@ app.options('/contacts', cors())
 }
 */
 app.get('/contacts', function (req, res) {
-  Contact.findAll({
-    attributes: ['createdAt', 'taskId', 'reservationId'],
-    include: [
-    {
-      model: AgeBracket,
-      attributes: [ 'bracket' ]
-    },
-    {
-      model: Subcategory,
-      attributes: [ 'subcategory' ]
-    }],
-    order: [ [ 'createdAt', 'DESC' ] ],
-    limit: 10
-  }).then(contacts => {
-    res.json(contacts.map(e =>
-      new Object({
-        "Date": e.createdAt,
-        "AgeBracket": e.AgeBracket.bracket,
-        "Subcategory": e.Subcategory.subcategory,
-        "TaskId": e.taskId,
-        "ReservationId": e.reservationId
-      })
-    ));
-  })
+  res.json({
+    "Message": "This is the GET request"
+  });
+  // Contact.findAll({
+  //   attributes: ['createdAt', 'taskId', 'reservationId'],
+  //   include: [
+  //   {
+  //     model: AgeBracket,
+  //     attributes: [ 'bracket' ]
+  //   },
+  //   {
+  //     model: Subcategory,
+  //     attributes: [ 'subcategory' ]
+  //   }],
+  //   order: [ [ 'createdAt', 'DESC' ] ],
+  //   limit: 10
+  // }).then(contacts => {
+  //   res.json(contacts.map(e =>
+  //     new Object({
+  //       "Date": e.createdAt,
+  //       "AgeBracket": e.AgeBracket.bracket,
+  //       "Subcategory": e.Subcategory.subcategory,
+  //       "TaskId": e.taskId,
+  //       "ReservationId": e.reservationId
+  //     })
+  //   ));
+  // })
 });
 
 
@@ -73,41 +76,44 @@ on error return 400 with
 TODO(nick): currently doing this with square brackets instead
 */
 app.post('/contacts', function(req, res) {
-  console.log(req.body);
-  // TODO(nick): Sanitize this so little bobby tables doesn't get us
-  var ageBracketPromise =
-    AgeBracket.findOne({where: { bracket: req.body.ageBracket }}); // cache eventually
-  var subcategoryPromise =
-    Subcategory.findOne({where: { subcategory: req.body.subcategory }});
-  Promise.all([ageBracketPromise, subcategoryPromise])
-    .then( ([ageBracket, subcategory]) => {
-      let errorArray = [];
-      if (ageBracket == null) {
-        errorArray.push("Invalid ageBracket: " + req.body.ageBracket);
-      }
-      if (subcategory == null) {
-        errorArray.push("Invalid subcategory: " + req.body.subcategory);
-      }
-      if (errorArray.length > 0) {
-        errorArray.push("error: BadDataException");
-        console.log(JSON.stringify(errorArray));
-        res.status(400).send(JSON.stringify(errorArray));
-        reject();
-      }
-      return Contact.create({
-        taskId: req.body.taskId,
-        reservationId: req.body.reservationId,
-        timestamp: req.body.timestamp,
-        AgeBracketId: ageBracket.id,
-        SubcategoryId: subcategory.id
-      });
-    })
-    .then(contact => {
-      let str = JSON.stringify(contact.toJSON());
-      console.log("contact = " + str);
-      res.json(str);
-    })
-    .catch( error => { console.log("request rejected: " + error); });
+  res.json({
+    "Message": "This is the POST request"
+  });
+  // console.log(req.body);
+  // // TODO(nick): Sanitize this so little bobby tables doesn't get us
+  // var ageBracketPromise =
+  //   AgeBracket.findOne({where: { bracket: req.body.ageBracket }}); // cache eventually
+  // var subcategoryPromise =
+  //   Subcategory.findOne({where: { subcategory: req.body.subcategory }});
+  // Promise.all([ageBracketPromise, subcategoryPromise])
+  //   .then( ([ageBracket, subcategory]) => {
+  //     let errorArray = [];
+  //     if (ageBracket == null) {
+  //       errorArray.push("Invalid ageBracket: " + req.body.ageBracket);
+  //     }
+  //     if (subcategory == null) {
+  //       errorArray.push("Invalid subcategory: " + req.body.subcategory);
+  //     }
+  //     if (errorArray.length > 0) {
+  //       errorArray.push("error: BadDataException");
+  //       console.log(JSON.stringify(errorArray));
+  //       res.status(400).send(JSON.stringify(errorArray));
+  //       reject();
+  //     }
+  //     return Contact.create({
+  //       taskId: req.body.taskId,
+  //       reservationId: req.body.reservationId,
+  //       timestamp: req.body.timestamp,
+  //       AgeBracketId: ageBracket.id,
+  //       SubcategoryId: subcategory.id
+  //     });
+  //   })
+  //   .then(contact => {
+  //     let str = JSON.stringify(contact.toJSON());
+  //     console.log("contact = " + str);
+  //     res.json(str);
+  //   })
+  //   .catch( error => { console.log("request rejected: " + error); });
 });
 
 module.exports = app;
