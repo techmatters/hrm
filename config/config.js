3c7b5c3f2d095c3aa0993fcfa6337653c9951c1d
Murilo Machado <murilovmachado@gmail.com>
2020-03-05 18:41:59 -0300

    Update config file to get values from ENV vars

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: "hrm",
+    password: null,
+    database: "hrmdb",
+    host: "localhost",
+    dialect: "postgres"
+  },
+  staging: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DBNAME,
+    host: process.env.RDS_HOSTNAME,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DBNAME,
+    host: process.env.RDS_HOSTNAME,
+    dialect: "postgres"
+  }
+}
