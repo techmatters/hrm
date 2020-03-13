819bdefc402b5c2f28012ee241146301e8281544
Murilo Machado <murilovmachado@gmail.com>
2020-03-13 17:25:28 -0300

    Config lint for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
-  "extends": ["airbnb-base", "prettier"],
-  "plugins": ["prettier"],
-  "rules": {
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+  ],
+  plugins: ["prettier", "jest"],
+  rules: {
     "prettier/prettier": ["error"],
     "no-console": "off",
-  }
+  },
+  env: {
+    "jest/globals": true,
+  },
 };
