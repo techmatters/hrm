e0442ea0999d5eddb736aa1fdb15e073defc9c4c
murilovmachado <murilovmachado@gmail.com>
2020-03-17 10:16:57 -0300

    Unit tests (#26)
    * Move searchContacts to ContactController
    
    * Move getContacts and createContact to ContactController
    
    * Use middleware for authorization
    
    * Handle async errors
    
    * Config lint for jest
    
    * Implement unit tests for searchContacts

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const createError = require('http-errors');
 const express = require('express');
+require('express-async-errors');
 const logger = require('morgan');
 const cors = require('cors');
 const Sequelize = require('sequelize');
-const parseISO = require('date-fns/parseISO');
-const startOfDay = require('date-fns/startOfDay');
-const endOfDay = require('date-fns/endOfDay');
-const isValid = require('date-fns/isValid');
 
 const app = express();
 
@@ -16,7 +13,6 @@ const user = process.env.RDS_USERNAME || 'hrm';
 const pass = process.env.RDS_PASSWORD || '';
 const apiKey = process.env.API_KEY;
 
-const { Op } = Sequelize;
 const version = '0.3.6';
 
 if (!apiKey) {
@@ -29,8 +25,9 @@ const sequelize = new Sequelize('hrmdb', 'hrm', pass, {
   host,
   dialect: 'postgres',
 });
+const ContactController = require('./controllers/contact-controller')(sequelize);
+
 console.log('After connect attempt');
-const Contact = require('./models/contact.js')(sequelize, Sequelize);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -45,268 +42,61 @@ app.get('/', (req, res) => {
 
 app.options('/contacts', cors());
 
-function checkAuthentication(req, res) {
+function unauthorized(res) {
+  const authorizationFailed = { error: 'Authorization failed' };
+  console.log(`[authorizationMiddleware]: ${JSON.stringify(authorizationFailed)}`);
+  res.status(401).json(authorizationFailed);
+}
+
+function authorizationMiddleware(req, res, next) {
+  if (!req || !req.headers || !req.headers.authorization) {
+    return unauthorized(res);
+  }
+
   const base64Key = Buffer.from(req.headers.authorization.replace('Basic ', ''), 'base64');
   if (base64Key.toString('ascii') !== apiKey) {
-    res.status(401).json({ error: 'Authentication failed' });
-  }
-}
-
-function formatNumber(number) {
-  if (number == null || number === 'Anonymous' || number === 'Customer') {
-    return number;
+    return unauthorized(res);
   }
 
-  const len = number.length;
-  return number.slice(0, 4) + 'X'.repeat(len - 7) + number.slice(len - 3);
+  return next();
 }
 
-function redact(form) {
-  if (!form) {
-    return form;
-  }
-
-  return {
-    ...form,
-    number: formatNumber(form.number),
-  };
-}
-
-function checkSearchParams(body, res) {
-  const {
-    helpline,
-    firstName,
-    lastName,
-    counselor,
-    phoneNumber,
-    dateFrom,
-    dateTo,
-    singleInput,
-  } = body;
-
-  /**
-   * Prettier currently enforces the operators (eg. ||) to the at the end of the line.
-   * It's much more readable when it's placed at the beggining of the line. But that's
-   * a current limitation of Prettier. There's a PR in place that will fix this in future
-   * versions of Prettier: https://github.com/prettier/prettier/pull/7111.
-   */
-  const anyValue =
-    helpline ||
-    firstName ||
-    lastName ||
-    counselor ||
-    phoneNumber ||
-    dateFrom ||
-    dateTo ||
-    singleInput;
-
-  if (!anyValue) {
-    res.json([]);
-  }
-}
-
-function buildSearchQueryObject(body) {
-  const {
-    helpline,
-    firstName,
-    lastName,
-    counselor,
-    phoneNumber,
-    dateFrom,
-    dateTo,
-    singleInput,
-  } = body;
-
-  const operator = singleInput ? Op.or : Op.and;
-  const isSingleInputValidDate = singleInput && isValid(parseISO(singleInput));
-  const compareDateFrom = dateFrom && !singleInput;
-  const compareDateTo = dateTo && !singleInput;
-
-  return {
-    where: {
-      [Op.and]: [
-        helpline && {
-          helpline,
-        },
-        {
-          [operator]: [
-            (firstName || singleInput) && {
-              'rawJson.childInformation.name.firstName': {
-                [Op.iLike]: singleInput || firstName,
-              },
-            },
-            (lastName || singleInput) && {
-              'rawJson.childInformation.name.lastName': {
-                [Op.iLike]: singleInput || lastName,
-              },
-            },
-            counselor && {
-              twilioWorkerId: counselor,
-            },
-            (phoneNumber || singleInput) && {
-              number: {
-                [Op.iLike]: `%${singleInput || phoneNumber}%`,
-              },
-            },
-            compareDateFrom && {
-              createdAt: {
-                [Op.gte]: startOfDay(parseISO(dateFrom)),
-              },
-            },
-            compareDateTo && {
-              createdAt: {
-                [Op.lte]: endOfDay(parseISO(dateTo)),
-              },
-            },
-            isSingleInputValidDate && {
-              [Op.and]: [
-                {
-                  createdAt: {
-                    [Op.gte]: startOfDay(parseISO(singleInput)),
-                  },
-                },
-                {
-                  createdAt: {
-                    [Op.lte]: endOfDay(parseISO(singleInput)),
-                  },
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  };
-}
-
-function isNullOrEmptyObject(obj) {
-  return obj == null || Object.keys(obj).length === 0;
-}
-
-function isValidContact(contact) {
-  return (
-    contact &&
-    contact.rawJson &&
-    !isNullOrEmptyObject(contact.rawJson.callType) &&
-    !isNullOrEmptyObject(contact.rawJson.childInformation) &&
-    !isNullOrEmptyObject(contact.rawJson.callerInformation) &&
-    !isNullOrEmptyObject(contact.rawJson.caseInformation)
-  );
-}
-
-function convertContactsToSearchResults(contacts) {
-  return contacts
-    .map(contact => {
-      if (!isValidContact(contact)) {
-        const contactJson = JSON.stringify(contact);
-        console.log(`Invalid Contact: ${contactJson}`);
-        return null;
-      }
-
-      const contactId = contact.id;
-      const dateTime = contact.createdAt;
-      const name = `${contact.rawJson.childInformation.name.firstName} ${contact.rawJson.childInformation.name.lastName}`;
-      const customerNumber = contact.number;
-      const { callType } = contact.rawJson;
-      const categories = 'TBD';
-      const counselor = contact.twilioWorkerId;
-      const notes = contact.rawJson.caseInformation.callSumary;
-
-      return {
-        contactId,
-        overview: {
-          dateTime,
-          name,
-          customerNumber,
-          callType,
-          categories,
-          counselor,
-          notes,
-        },
-        details: redact(contact.rawJson),
-      };
-    })
-    .filter(contact => contact);
-}
+app.use(authorizationMiddleware);
 
 // run with node app.js and hit curl localhost:8080/contacts/
-app.get('/contacts', (req, res) => {
-  checkAuthentication(req, res);
-  const queryObject = {
-    order: [['createdAt', 'DESC']],
-    limit: 10,
-  };
-  if (req.query.queueName) {
-    queryObject.where = {
-      queueName: {
-        [Op.like]: `${req.query.queueName}%`,
-      },
-    };
-  }
-  Contact.findAll(queryObject).then(contacts => {
-    res.json(
-      contacts.map(e => ({
-        id: e.id,
-        Date: e.createdAt,
-        FormData: redact(e.rawJson),
-        twilioWorkerId: e.twilioWorkerId,
-        helpline: e.helpline,
-        queueName: e.queueName,
-        number: formatNumber(e.number),
-        channel: e.channel,
-        conversationDuration: e.conversationDuration,
-      })),
-    );
-  });
+app.get('/contacts', async (req, res) => {
+  const contacts = await ContactController.getContacts(req.query);
+  res.json(contacts);
 });
 
-app.post('/contacts/search', (req, res) => {
-  checkAuthentication(req, res);
-  checkSearchParams(req.body, res);
-
-  const queryObject = buildSearchQueryObject(req.body);
-
-  Contact.findAll(queryObject)
-    .then(contacts => res.json(convertContactsToSearchResults(contacts)))
-    .catch(error => console.log(`request rejected: ${error}`));
+app.post('/contacts/search', async (req, res) => {
+  const searchResults = await ContactController.searchContacts(req.body);
+  res.json(searchResults);
 });
 
 // example: curl -XPOST -H'Content-Type: application/json' localhost:3000/contacts -d'{"hi": 2}'
-app.post('/contacts', (req, res) => {
-  checkAuthentication(req, res);
-  console.log(req.body);
-  // TODO(nick): Sanitize this so little bobby tables doesn't get us
-  const contactRecord = {
-    rawJson: req.body.form,
-    twilioWorkerId: req.body.twilioWorkerId || '',
-    helpline: req.body.helpline || '',
-    queueName: req.body.queueName || req.body.form.queueName,
-    number: req.body.number || '',
-    channel: req.body.channel || '',
-    conversationDuration: req.body.conversationDuration,
-  };
-
-  Contact.create(contactRecord)
-    .then(contact => {
-      const str = JSON.stringify(contact.toJSON());
-      console.log(`contact = ${str}`);
-      res.json(str);
-    })
-    .catch(error => console.log(`request rejected: ${error}`));
+app.post('/contacts', async (req, res) => {
+  const contact = await ContactController.createContact(req.body);
+  res.json(contact);
 });
 
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
+  console.log(err);
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  const error =
+    req.app.get('env') === 'development' ? { message: err.message, error: err.stack } : {};
+
   res.status(err.status || 500);
-  res.render('error');
+  res.json(error);
+  next();
 });
 
 console.log(`${new Date(Date.now()).toLocaleString()}: app.js has been created`);
---
+++ controllers/contact-controller.js
@@ -0,0 +1,244 @@
+const Sequelize = require('sequelize');
+const parseISO = require('date-fns/parseISO');
+const startOfDay = require('date-fns/startOfDay');
+const endOfDay = require('date-fns/endOfDay');
+const isValid = require('date-fns/isValid');
+
+const contactModel = require('../models/contact.js');
+
+const { Op } = Sequelize;
+
+function formatNumber(number) {
+  if (number == null || number === 'Anonymous' || number === 'Customer') {
+    return number;
+  }
+
+  const len = number.length;
+  return number.slice(0, 4) + 'X'.repeat(len - 7) + number.slice(len - 3);
+}
+
+function redact(form) {
+  if (!form) {
+    return form;
+  }
+
+  return {
+    ...form,
+    number: formatNumber(form.number),
+  };
+}
+
+function isEmptySearchParams(body) {
+  const {
+    helpline,
+    firstName,
+    lastName,
+    counselor,
+    phoneNumber,
+    dateFrom,
+    dateTo,
+    singleInput,
+  } = body;
+
+  /**
+   * Prettier currently enforces the operators (eg. ||) to the at the end of the line.
+   * It's much more readable when it's placed at the beggining of the line. But that's
+   * a current limitation of Prettier. There's a PR in place that will fix this in future
+   * versions of Prettier: https://github.com/prettier/prettier/pull/7111.
+   */
+  const anyValue =
+    helpline ||
+    firstName ||
+    lastName ||
+    counselor ||
+    phoneNumber ||
+    dateFrom ||
+    dateTo ||
+    singleInput;
+
+  return !anyValue;
+}
+
+const orUndefined = value => value || undefined;
+
+function buildSearchQueryObject(body) {
+  const {
+    helpline,
+    firstName,
+    lastName,
+    counselor,
+    phoneNumber,
+    dateFrom,
+    dateTo,
+    singleInput,
+  } = body;
+
+  const operator = singleInput ? Op.or : Op.and;
+  const isSingleInputValidDate = orUndefined(singleInput && isValid(parseISO(singleInput)));
+  const compareCounselor = orUndefined(counselor && !singleInput);
+  const compareDateFrom = orUndefined(dateFrom && !singleInput);
+  const compareDateTo = orUndefined(dateTo && !singleInput);
+
+  return {
+    where: {
+      [Op.and]: [
+        helpline && {
+          helpline,
+        },
+        {
+          [operator]: [
+            (firstName || singleInput) && {
+              'rawJson.childInformation.name.firstName': {
+                [Op.iLike]: singleInput || firstName,
+              },
+            },
+            (lastName || singleInput) && {
+              'rawJson.childInformation.name.lastName': {
+                [Op.iLike]: singleInput || lastName,
+              },
+            },
+            compareCounselor && {
+              twilioWorkerId: counselor,
+            },
+            (phoneNumber || singleInput) && {
+              number: {
+                [Op.iLike]: `%${singleInput || phoneNumber}%`,
+              },
+            },
+            compareDateFrom && {
+              createdAt: {
+                [Op.gte]: startOfDay(parseISO(dateFrom)),
+              },
+            },
+            compareDateTo && {
+              createdAt: {
+                [Op.lte]: endOfDay(parseISO(dateTo)),
+              },
+            },
+            isSingleInputValidDate && {
+              [Op.and]: [
+                {
+                  createdAt: {
+                    [Op.gte]: startOfDay(parseISO(singleInput)),
+                  },
+                },
+                {
+                  createdAt: {
+                    [Op.lte]: endOfDay(parseISO(singleInput)),
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  };
+}
+
+function isNullOrEmptyObject(obj) {
+  return obj == null || Object.keys(obj).length === 0;
+}
+
+function isValidContact(contact) {
+  return (
+    contact &&
+    contact.rawJson &&
+    !isNullOrEmptyObject(contact.rawJson.callType) &&
+    !isNullOrEmptyObject(contact.rawJson.childInformation) &&
+    !isNullOrEmptyObject(contact.rawJson.callerInformation) &&
+    !isNullOrEmptyObject(contact.rawJson.caseInformation)
+  );
+}
+
+function convertContactsToSearchResults(contacts) {
+  return contacts
+    .map(contact => {
+      if (!isValidContact(contact)) {
+        const contactJson = JSON.stringify(contact);
+        console.log(`Invalid Contact: ${contactJson}`);
+        return null;
+      }
+
+      const contactId = contact.id;
+      const dateTime = contact.createdAt;
+      const name = `${contact.rawJson.childInformation.name.firstName} ${contact.rawJson.childInformation.name.lastName}`;
+      const customerNumber = formatNumber(contact.number);
+      const { callType } = contact.rawJson;
+      const categories = 'TBD';
+      const counselor = contact.twilioWorkerId;
+      const notes = contact.rawJson.caseInformation.callSummary;
+
+      return {
+        contactId,
+        overview: {
+          dateTime,
+          name,
+          customerNumber,
+          callType,
+          categories,
+          counselor,
+          notes,
+        },
+        details: redact(contact.rawJson),
+      };
+    })
+    .filter(contact => contact);
+}
+
+const ContactController = sequelize => {
+  const Contact = contactModel(sequelize, Sequelize);
+
+  const searchContacts = async body => {
+    if (isEmptySearchParams(body)) {
+      return [];
+    }
+    const queryObject = buildSearchQueryObject(body);
+    const contacts = await Contact.findAll(queryObject);
+    return convertContactsToSearchResults(contacts);
+  };
+
+  const getContacts = async query => {
+    const queryObject = {
+      order: [['createdAt', 'DESC']],
+      limit: 10,
+    };
+    if (query.queueName) {
+      queryObject.where = {
+        queueName: {
+          [Op.like]: `${query.queueName}%`,
+        },
+      };
+    }
+    const contacts = await Contact.findAll(queryObject);
+    return contacts.map(e => ({
+      id: e.id,
+      Date: e.createdAt,
+      FormData: redact(e.rawJson),
+      twilioWorkerId: e.twilioWorkerId,
+      helpline: e.helpline,
+      queueName: e.queueName,
+      number: formatNumber(e.number),
+      channel: e.channel,
+      conversationDuration: e.conversationDuration,
+    }));
+  };
+  const createContact = async body => {
+    const contactRecord = {
+      rawJson: body.form,
+      twilioWorkerId: body.twilioWorkerId || '',
+      helpline: body.helpline || '',
+      queueName: body.queueName || body.form.queueName,
+      number: body.number || '',
+      channel: body.channel || '',
+      conversationDuration: body.conversationDuration,
+    };
+
+    const contact = await Contact.create(contactRecord);
+    return contact;
+  };
+
+  return { searchContacts, getContacts, createContact };
+};
+
+module.exports = ContactController;
