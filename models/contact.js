26449e07aab9ab2bb1c5457f930c2dd95ceddfe8
Murilo Machado <murilovmachado@gmail.com>
2020-05-06 17:21:13 -0300

    DB cleanup

--- models/contact.js
+++ models/contact.js
@@ -1,8 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const Contact = sequelize.define('Contact', {
-    timestamp: DataTypes.BIGINT,
     taskId: DataTypes.STRING,
-    reservationId: DataTypes.STRING,
     rawJson: DataTypes.JSON,
     queueName: DataTypes.STRING,
     twilioWorkerId: DataTypes.STRING,
@@ -12,11 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     conversationDuration: DataTypes.INTEGER,
   });
 
-  Contact.associate = models => {
-    Contact.belongsTo(models.Case, { foreignKey: 'caseId' });
-    Contact.belongsTo(models.AgeBracket, { foreignKey: 'AgeBracketId' });
-    Contact.belongsTo(models.Subcategory, { foreignKey: 'SubcategoryId' });
-  };
+  Contact.associate = models => Contact.belongsTo(models.Case, { foreignKey: 'caseId' });
 
   return Contact;
 };
