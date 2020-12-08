935df45ae3bf8610494cf3cfd4b3d552adc9a1f4
gpaoloni <paolonigianfranco@gmail.com>
2020-12-08 11:55:06 -0300

    Fixed test

--- tests/controllers/activities.test.js
+++ tests/controllers/activities.test.js
@@ -94,6 +94,7 @@ describe('getActivity', () => {
 
     const activity = getActivity(caseAudit, relatedContacts);
     const expectedActivity = {
+      contactId: 1,
       date: createdAt,
       type: 'facebook',
       text: 'Child summary',
