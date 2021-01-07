b1a3c5bdd3d5ce104830cf3139cc5f0444e775b2
gpaoloni <paolonigianfranco@gmail.com>
2021-01-07 16:27:36 -0300

    Grouped case audit controllers together

--- controllers/case-audit-controller.js
+++ controllers/case-audit-controller.js
@@ -27,7 +27,53 @@ const CaseAuditController = CaseAudit => {
     return activities;
   };
 
-  return { getAuditsForCase, getActivities, getContactIdsFromCaseAudits };
+  const createCaseAuditFromContact = async (
+    initialContactsFunction,
+    contactInstance,
+    caseFromDB,
+    transaction,
+  ) => {
+    if (!caseFromDB) return;
+
+    const contacts = await caseFromDB.getConnectedContacts();
+    const contactsId = contacts.map(contact => contact.dataValues.id);
+    const previousValue = {
+      ...caseFromDB.dataValues,
+      contacts: initialContactsFunction(contactsId, contactInstance.dataValues.id),
+    };
+    const newValue = {
+      ...caseFromDB.dataValues,
+      contacts: contactsId,
+    };
+    const caseAuditRecord = {
+      caseId: caseFromDB.dataValues.id,
+      twilioWorkerId: contactInstance.dataValues.twilioWorkerId,
+      accountSid: contactInstance.dataValues.accountSid,
+      previousValue,
+      newValue,
+    };
+
+    await CaseAudit.create(caseAuditRecord, { transaction });
+  };
+
+  const createCaseAuditFromCase = async (previousValue, newValue, transaction) => {
+    const caseAuditRecord = {
+      caseId: newValue.id,
+      twilioWorkerId: newValue.twilioWorkerId,
+      accountSid: newValue.accountSid,
+      previousValue,
+      newValue,
+    };
+    await CaseAudit.create(caseAuditRecord, { transaction });
+  };
+
+  return {
+    getAuditsForCase,
+    getActivities,
+    getContactIdsFromCaseAudits,
+    createCaseAuditFromContact,
+    createCaseAuditFromCase,
+  };
 };
 
 module.exports = CaseAuditController;
