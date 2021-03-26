d71a03d1e1baf4d42fb255211de95c2ed6534b9b
Nick Hurlburt <nick@techmatters.org>
2021-03-26 09:51:22 -0700

    Simplify to just use manage

--- permissions/rules/open.js
+++ permissions/rules/open.js
@@ -1,6 +1,4 @@
 const CanCan = require('cancan');
-const { isCounselorWhoCreated, isSupervisor, isCaseOpen } = require('./helpers');
-const Actions = require('../actions');
 const User = require('../user');
 const models = require('../../models');
 
@@ -9,81 +7,7 @@ const { can, allow } = cancan;
 
 const { Case } = models;
 
-allow(
-  User,
-  Actions.CLOSE_CASE,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(User, Actions.REOPEN_CASE, Case, true);
-
-allow(
-  User,
-  Actions.ADD_NOTE,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(User, Actions.EDIT_NOTE, Case, user => true);
-
-allow(
-  User,
-  Actions.ADD_REFERRAL,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(
-  User,
-  Actions.EDIT_REFERRAL,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(
-  User,
-  Actions.ADD_HOUSEHOLD,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(
-  User,
-  Actions.EDIT_HOUSEHOLD,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(
-  User,
-  Actions.ADD_PERPETRATOR,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(
-  User,
-  Actions.EDIT_PERPETRATOR,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(
-  User,
-  Actions.ADD_INCIDENT,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(
-  User,
-  Actions.EDIT_INCIDENT,
-  Case,
-  (user, caseObj) => true,
-);
-
-allow(User, Actions.EDIT_CASE_SUMMARY, Case, user => true);
+allow(User, 'manage', Case);
 
 const applyPermissions = req => {
   req.can = can;
