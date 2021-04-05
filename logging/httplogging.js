d029ea029396bdb82f9fa2bcdf62551cec5e9124
Andrés Virga <andvirga@gmail.com>
2021-04-05 14:48:12 -0300

    Modified morgan configuration to remove log coloring (#106)

---
+++ logging/httplogging.js
@@ -0,0 +1,11 @@
+const morgan = require('morgan');
+
+/**
+ * Basic HTTP Logger that shows information about each request.
+ * Example: 200 - GET /contacts HTTP/1.1 - time: 10 ms length: 95 - Agent: curl/7.58.0
+ */
+const httpLogger = morgan(
+  ':status - :method :url HTTP/:http-version - time: :response-time ms - length: :res[content-length] - Agent: :user-agent',
+);
+
+module.exports = httpLogger;
