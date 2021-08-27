14c47143e137f302b37c101514c3a2ebc52acfc8
gpaoloni <paolonigianfranco@gmail.com>
2021-08-27 18:26:57 -0300

    Added post survey controllers

---
+++ controllers/post-survey-controller.js
@@ -0,0 +1,65 @@
+const Sequelize = require('sequelize');
+
+const { Op } = Sequelize;
+
+const PostSurveyController = PostSurvey => {
+  /**
+   *
+   * @param {{}} query
+   * @param {string} accountSid
+   */
+  const getPostSurveys = async (query, accountSid) => {
+    // const {} = query;
+    const queryObject = {
+      limit: 10,
+    };
+
+    queryObject.where = {
+      [Op.and]: [accountSid && { accountSid }],
+    };
+
+    const postSurveys = await PostSurvey.findAll(queryObject);
+    return postSurveys;
+  };
+
+  /**
+   *
+   * @param {string} contactTaskId
+   * @param {string} accountSid
+   */
+  const getPostSurveysByContactTaskId = async (contactTaskId, accountSid) => {
+    const queryObject = {
+      where: {
+        [Op.and]: [accountSid && { accountSid }, contactTaskId && { contactTaskId }],
+      },
+    };
+
+    return PostSurvey.findAll(queryObject);
+  };
+
+  /**
+   *
+   * @param {{ helpline: string; taskId: string; contactTaskId: string; data: {} }} body
+   * @param {string} accountSid
+   */
+  const createPostSurvey = async (body, accountSid) => {
+    const record = {
+      helpline: body.helpline || '',
+      accountSid: accountSid || '',
+      taskId: body.taskId || '',
+      contactTaskId: body.contactTaskId || '',
+      data: body.data,
+    };
+
+    const postSurvey = await PostSurvey.create(record);
+    return postSurvey;
+  };
+
+  return {
+    getPostSurveys,
+    getPostSurveysByContactTaskId,
+    createPostSurvey,
+  };
+};
+
+module.exports = PostSurveyController;
