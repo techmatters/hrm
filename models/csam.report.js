70aa14e0a38d80f32058d9ea0791b7490bda00ee
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2021-11-22 16:34:04 -0300

    Renamed foreing key to contactId

--- models/csam.report.js
+++ models/csam.report.js
@@ -2,12 +2,12 @@ module.exports = (sequelize, DataTypes) => {
   const CSAMReport = sequelize.define('CSAMReport', {
     accountSid: DataTypes.STRING,
     twilioWorkerId: DataTypes.STRING, // The worker who submited the report
-    contactTaskId: DataTypes.STRING, // The contact associated to this report (if any)
+    contactId: DataTypes.STRING, // The contact associated to this report (if any)
     csamReportId: DataTypes.STRING, // The id returned from the exteral API (like IWF)
   });
 
   CSAMReport.associate = models => {
-    CSAMReport.belongsTo(models.Contact, { foreingKey: 'contactTaskId' });
+    CSAMReport.belongsTo(models.Contact, { foreingKey: 'contactId' });
   };
 
   return CSAMReport;
