7f06194003c7f50e30696d39e89ca9258e6ffffd
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2021-11-23 11:11:24 -0300

    Fixed models with association

--- models/contact.js
+++ models/contact.js
@@ -74,7 +74,7 @@ module.exports = (sequelize, DataTypes) => {
   Contact.associate = models => {
     Contact.belongsTo(models.Case, { foreignKey: 'caseId' });
     Contact.hasMany(models.PostSurvey, { foreignKey: 'contactTaskId' });
-    Contact.hasMany(models.CSAMReport, { foreignKey: 'contactId' });
+    Contact.hasMany(models.CSAMReport, { foreignKey: 'contactId', as: 'CSAMReports' });
   };
 
   /**
--- models/csam-report.js
+++ models/csam-report.js
@@ -2,12 +2,12 @@ module.exports = (sequelize, DataTypes) => {
   const CSAMReport = sequelize.define('CSAMReport', {
     accountSid: DataTypes.STRING,
     twilioWorkerId: DataTypes.STRING, // The worker who submited the report
-    contactId: DataTypes.STRING, // The contact associated to this report (if any)
     csamReportId: DataTypes.STRING, // The id returned from the exteral API (like IWF)
+    contactId: DataTypes.INTEGER, // The contact associated to this report (if any)
   });
 
   CSAMReport.associate = models => {
-    CSAMReport.belongsTo(models.Contact, { foreingKey: 'contactId' });
+    CSAMReport.belongsTo(models.Contact, { foreignKey: 'contactId' });
   };
 
   return CSAMReport;
