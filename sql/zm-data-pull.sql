8eacdc31ee6d2ce6d0b6aba0b5813f62f04039ed
Murilo Machado <murilovmachado@gmail.com>
2021-12-07 13:57:25 +0000

    ZM Data Pull script

---
+++ sql/zm-data-pull.sql
@@ -0,0 +1,22 @@
+SELECT 
+	"createdAt",
+	"twilioWorkerId" as counselor, -- TODO: print counselor name in the future
+	"timeOfContact",
+	"rawJson"->'childInformation'->>'gender' as gender,
+	"rawJson"->'childInformation'->>'age' as age,
+	"rawJson"->'childInformation'->>'province' as province,
+	"rawJson"->'childInformation'->>'district' as district,
+	"rawJson"->'childInformation'->>'phone1' as phone1,
+	"rawJson"->'caseInformation'->>'callSummary' as callsummary,
+	string_agg('[' || categories.key || ']: ' || subcategories.key, ', ') as categories
+FROM
+  public."Contacts" contacts
+  left join lateral jsonb_each("rawJson"->'caseInformation'->'categories') categories on true
+  left join lateral jsonb_each_text(categories.value) subcategories on true
+where
+  "createdAt" >= '2021-11-01' -- Fill dateFrom
+  and "createdAt" <= '2021-11-30' -- Fill dateTo
+  and "accountSid" = 'XXXXXXXXXX' -- Fill accountSid
+  and subcategories.value = 'true'
+group by "createdAt", "twilioWorkerId", "timeOfContact", "rawJson" 
+order by "createdAt";
