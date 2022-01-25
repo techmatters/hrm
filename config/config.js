d19d254ea8a0556f0f12b92ecb16c1280665874a
Stephen Hand <steveh@techmatters.org>
2022-01-25 18:25:46 +0000

    Fixed issue with DB records for contacts route tests being added in a non deterministic order. Added docker compose config specifically for a DB to run the tests against. Fixed issue with server sessions not being fully shut down at the end of test specs.

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ module.exports = {
     password: process.env.RDS_PASSWORD || null,
     database: process.env.RDS_DBNAME || 'hrmdb',
     host: process.env.RDS_HOSTNAME || 'localhost',
+    port: process.env.POSTGRES_PORT || 5432,
     dialect: 'postgres',
   },
   staging: {
