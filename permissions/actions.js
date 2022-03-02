b281a2929c93592c58ef605d3c6b99a71b6aa374
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-03-02 19:31:14 -0300

    Rework on Case permissions framework to support edit actions

--- permissions/actions.js
+++ permissions/actions.js
@@ -22,48 +22,73 @@ const VIEW_POST_SURVEY = 'viewPostSurvey';
 const NEW_PROPERTY = 'N';
 const EDITED_PROPERTY = 'E';
 const ARRAY_CHANGED = 'A';
+// eslint-disable-next-line no-unused-vars
+const DELETED_PROPERTY = 'D';
 
+/**
+ * Compares the two first components of a path to decide if they refer to the same "case item type" under case.info (e.g. if it's editing notes, perpetrators, etc)
+ * Example paths are like:
+ *   [ 'info', 'perpetrators' ] when adding a new perpetrator
+ *   [ 'info', 'perpetrators', 0, 'perpetrator', 'phone1' ] when editin an existing perpetrator
+ */
 const isPathEqual = (left, right) => left[0] === right[0] && left[1] === right[1];
 
-const isAddOrEditKind = kind => [NEW_PROPERTY, EDITED_PROPERTY, ARRAY_CHANGED].includes(kind);
+const isNewOrAddKind = kind => kind === NEW_PROPERTY || kind === ARRAY_CHANGED;
+const isEditKind = kind => kind === EDITED_PROPERTY;
+const isAddOrEditKind = kind => isNewOrAddKind(kind) || isEditKind(kind);
 
 const isEditCaseSummary = change =>
   isAddOrEditKind(change.kind) && isPathEqual(change.path, ['info', 'summary']);
 
 const isCloseCase = change =>
-  change.kind === EDITED_PROPERTY &&
-  isPathEqual(change.path, ['status']) &&
-  change.rhs === 'closed';
+  isEditKind(change.kind) && isPathEqual(change.path, ['status']) && change.rhs === 'closed';
 
 const isReopenCase = change =>
-  change.kind === EDITED_PROPERTY &&
+  isEditKind(change.kind) &&
   isPathEqual(change.path, ['status']) &&
   change.lhs === 'closed' &&
   change.rhs !== 'closed';
 
 const isCaseStatusTransition = change =>
-  change.kind === EDITED_PROPERTY &&
+  isEditKind(change.kind) &&
   isPathEqual(change.path, ['status']) &&
   change.lhs !== 'closed' &&
   change.rhs !== 'closed';
 
 const isAddNote = change =>
-  isAddOrEditKind(change.kind) && isPathEqual(change.path, ['info', 'notes']);
+  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'notes']);
 
 const isAddReferral = change =>
-  isAddOrEditKind(change.kind) && isPathEqual(change.path, ['info', 'referrals']);
+  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'referrals']);
 
 const isAddHousehold = change =>
-  isAddOrEditKind(change.kind) && isPathEqual(change.path, ['info', 'households']);
+  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'households']);
 
 const isAddPerpetrator = change =>
-  isAddOrEditKind(change.kind) && isPathEqual(change.path, ['info', 'perpetrators']);
+  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'perpetrators']);
 
 const isAddIncident = change =>
-  isAddOrEditKind(change.kind) && isPathEqual(change.path, ['info', 'incidents']);
+  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'incidents']);
 
 const isAddDocument = change =>
-  isAddOrEditKind(change.kind) && isPathEqual(change.path, ['info', 'documents']);
+  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'documents']);
+
+const isEditNote = change => isEditKind(change.kind) && isPathEqual(change.path, ['info', 'notes']);
+
+const isEditReferral = change =>
+  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'referrals']);
+
+const isEditHousehold = change =>
+  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'households']);
+
+const isEditPerpetrator = change =>
+  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'perpetrators']);
+
+const isEditIncident = change =>
+  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'incidents']);
+
+const isEditDocument = change =>
+  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'documents']);
 
 /**
  * This function compares the original object and the object with the updated values
@@ -73,9 +98,17 @@ const isAddDocument = change =>
  * @returns
  */
 const getActions = (original, updated) => {
+  // Filter out the topmost properties not included in the payload to avoid false DELETED_PROPERTY (as we send Partial<Case> from the frontend)
+  const partialOriginal = Object.keys(updated).reduce(
+    (accum, currentKey) =>
+      Object.prototype.hasOwnProperty.call(original, currentKey)
+        ? { ...accum, [currentKey]: original[currentKey] }
+        : accum,
+    {},
+  );
   const ignoredProperties = ['createdAt', 'updatedAt', 'connectedContacts'];
   const preFilter = (path, key) => ignoredProperties.includes(key);
-  const changes = diff(original, updated, preFilter);
+  const changes = diff(partialOriginal, updated, preFilter);
 
   const actions = [];
   if (changes) {
@@ -90,6 +123,12 @@ const getActions = (original, updated) => {
       if (isAddIncident(change)) actions.push(ADD_INCIDENT);
       if (isAddDocument(change)) actions.push(ADD_DOCUMENT);
       if (isEditCaseSummary(change)) actions.push(EDIT_CASE_SUMMARY);
+      if (isEditNote(change)) actions.push(EDIT_NOTE);
+      if (isEditReferral(change)) actions.push(EDIT_REFERRAL);
+      if (isEditHousehold(change)) actions.push(EDIT_HOUSEHOLD);
+      if (isEditPerpetrator(change)) actions.push(EDIT_PERPETRATOR);
+      if (isEditIncident(change)) actions.push(EDIT_INCIDENT);
+      if (isEditDocument(change)) actions.push(EDIT_DOCUMENT);
     });
   }
 
