e661d6c33862842125a2ca5564c2e5eee86f1157
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-03-03 20:07:02 -0300

    Refined the cases where we check actions over cases

--- permissions/actions.js
+++ permissions/actions.js
@@ -26,73 +26,80 @@ const ARRAY_CHANGED = 'A';
 const DELETED_PROPERTY = 'D';
 
 /**
- * Compares the two first components of a path to decide if they refer to the same "case item type" under case.info (e.g. if it's editing notes, perpetrators, etc)
+ * @param {string[]} target
+ * @param {string[]} changePath
+ * Compares the target.length first components of a change path to decide if they refer to the same "case item type" under case.info (e.g. if it's editing the status, info (notes, perpetrators, etc))
  * Example paths are like:
  *   [ 'info', 'perpetrators' ] when adding a new perpetrator
  *   [ 'info', 'perpetrators', 0, 'perpetrator', 'phone1' ] when editin an existing perpetrator
  */
-const isPathEqual = (left, right) => left[0] === right[0] && left[1] === right[1];
+const isPathTarget = (target, changePath) =>
+  target.every((value, index) => changePath[index] === value);
+
+const isPathTargetsStatus = path => isPathTarget(['status'], path);
+const isPathTargetsInfo = path => isPathTarget(['info'], path);
 
 const isNewOrAddKind = kind => kind === NEW_PROPERTY || kind === ARRAY_CHANGED;
 const isEditKind = kind => kind === EDITED_PROPERTY;
 const isAddOrEditKind = kind => isNewOrAddKind(kind) || isEditKind(kind);
 
 const isCloseCase = change =>
-  isEditKind(change.kind) && isPathEqual(change.path, ['status']) && change.rhs === 'closed';
+  isEditKind(change.kind) && isPathTargetsStatus(change.path) && change.rhs === 'closed';
 
 const isReopenCase = change =>
   isEditKind(change.kind) &&
-  isPathEqual(change.path, ['status']) &&
+  isPathTargetsStatus(change.path) &&
   change.lhs === 'closed' &&
   change.rhs !== 'closed';
 
 const isCaseStatusTransition = change =>
   isEditKind(change.kind) &&
-  isPathEqual(change.path, ['status']) &&
+  isPathTargetsStatus(change.path) &&
   change.lhs !== 'closed' &&
   change.rhs !== 'closed';
 
 const isAddNote = change =>
-  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'notes']);
+  isNewOrAddKind(change.kind) && isPathTarget(['info', 'notes'], change.path);
 
 const isAddReferral = change =>
-  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'referrals']);
+  isNewOrAddKind(change.kind) && isPathTarget(['info', 'referrals'], change.path);
 
 const isAddHousehold = change =>
-  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'households']);
+  isNewOrAddKind(change.kind) && isPathTarget(['info', 'households'], change.path);
 
 const isAddPerpetrator = change =>
-  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'perpetrators']);
+  isNewOrAddKind(change.kind) && isPathTarget(['info', 'perpetrators'], change.path);
 
 const isAddIncident = change =>
-  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'incidents']);
+  isNewOrAddKind(change.kind) && isPathTarget(['info', 'incidents'], change.path);
 
 const isAddDocument = change =>
-  isNewOrAddKind(change.kind) && isPathEqual(change.path, ['info', 'documents']);
+  isNewOrAddKind(change.kind) && isPathTarget(['info', 'documents'], change.path);
 
-const isEditNote = change => isEditKind(change.kind) && isPathEqual(change.path, ['info', 'notes']);
+const isEditNote = change =>
+  isEditKind(change.kind) && isPathTarget(['info', 'notes'], change.path);
 
 const isEditReferral = change =>
-  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'referrals']);
+  isEditKind(change.kind) && isPathTarget(['info', 'referrals'], change.path);
 
 const isEditHousehold = change =>
-  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'households']);
+  isEditKind(change.kind) && isPathTarget(['info', 'households'], change.path);
 
 const isEditPerpetrator = change =>
-  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'perpetrators']);
+  isEditKind(change.kind) && isPathTarget(['info', 'perpetrators'], change.path);
 
 const isEditIncident = change =>
-  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'incidents']);
+  isEditKind(change.kind) && isPathTarget(['info', 'incidents'], change.path);
 
 const isEditDocument = change =>
-  isEditKind(change.kind) && isPathEqual(change.path, ['info', 'documents']);
+  isEditKind(change.kind) && isPathTarget(['info', 'documents'], change.path);
 
 const isEditCaseSummary = change =>
-  isAddOrEditKind(change.kind) && isPathEqual(change.path, ['info', 'summary']);
+  isAddOrEditKind(change.kind) && isPathTarget(['info', 'summary'], change.path);
 
 /**
  * This function compares the original object and the object with the updated values
- * to decide what actions it's trying to do, e.g.: [ADD_NOTE, ADD_REFERRAL]
+ * to decide what actions it's trying to do, e.g.: [ADD_NOTE, EDIT_REFERRAL]
  * @param {*} original the original object from DB
  * @param {*} updated the object with the updated values
  * @returns
@@ -113,22 +120,27 @@ const getActions = (original, updated) => {
   const actions = [];
   if (changes) {
     changes.forEach(change => {
-      if (isCloseCase(change)) actions.push(CLOSE_CASE);
-      if (isReopenCase(change)) actions.push(REOPEN_CASE);
-      if (isCaseStatusTransition(change)) actions.push(CASE_STATUS_TRANSITION);
-      if (isAddNote(change)) actions.push(ADD_NOTE);
-      if (isAddReferral(change)) actions.push(ADD_REFERRAL);
-      if (isAddHousehold(change)) actions.push(ADD_HOUSEHOLD);
-      if (isAddPerpetrator(change)) actions.push(ADD_PERPETRATOR);
-      if (isAddIncident(change)) actions.push(ADD_INCIDENT);
-      if (isAddDocument(change)) actions.push(ADD_DOCUMENT);
-      if (isEditCaseSummary(change)) actions.push(EDIT_CASE_SUMMARY);
-      if (isEditNote(change)) actions.push(EDIT_NOTE);
-      if (isEditReferral(change)) actions.push(EDIT_REFERRAL);
-      if (isEditHousehold(change)) actions.push(EDIT_HOUSEHOLD);
-      if (isEditPerpetrator(change)) actions.push(EDIT_PERPETRATOR);
-      if (isEditIncident(change)) actions.push(EDIT_INCIDENT);
-      if (isEditDocument(change)) actions.push(EDIT_DOCUMENT);
+      if (isPathTargetsStatus(change.path)) {
+        if (isCloseCase(change)) actions.push(CLOSE_CASE);
+        if (isReopenCase(change)) actions.push(REOPEN_CASE);
+        if (isCaseStatusTransition(change)) actions.push(CASE_STATUS_TRANSITION);
+      }
+
+      if (isPathTargetsInfo(change.path)) {
+        if (isAddNote(change)) actions.push(ADD_NOTE);
+        if (isAddReferral(change)) actions.push(ADD_REFERRAL);
+        if (isAddHousehold(change)) actions.push(ADD_HOUSEHOLD);
+        if (isAddPerpetrator(change)) actions.push(ADD_PERPETRATOR);
+        if (isAddIncident(change)) actions.push(ADD_INCIDENT);
+        if (isAddDocument(change)) actions.push(ADD_DOCUMENT);
+        if (isEditCaseSummary(change)) actions.push(EDIT_CASE_SUMMARY);
+        if (isEditNote(change)) actions.push(EDIT_NOTE);
+        if (isEditReferral(change)) actions.push(EDIT_REFERRAL);
+        if (isEditHousehold(change)) actions.push(EDIT_HOUSEHOLD);
+        if (isEditPerpetrator(change)) actions.push(EDIT_PERPETRATOR);
+        if (isEditIncident(change)) actions.push(EDIT_INCIDENT);
+        if (isEditDocument(change)) actions.push(EDIT_DOCUMENT);
+      }
     });
   }
 
