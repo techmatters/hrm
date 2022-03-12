9797b73bc674402f99c9b8420fe58369c2e388d9
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-03-12 02:07:32 -0300

    Reworked setupPermissions to use json rules + added open and zm

--- permissions/index.js
+++ permissions/index.js
@@ -1,40 +1,73 @@
 const User = require('./user');
 const { SafeRouter, publicEndpoint } = require('./safe-router');
-const { applyPermissions: applyZmPermissions } = require('./rules/zm');
-const { applyPermissions: applyZaPermissions } = require('./rules/za');
-const { applyPermissions: applyEtPermissions } = require('./rules/et');
-const { applyPermissions: applyMwPermissions } = require('./rules/mw');
-const { applyPermissions: applyBrPermissions } = require('./rules/br');
-const { applyPermissions: applyInPermissions } = require('./rules/in');
-const { applyPermissions: applyJmPermissions } = require('./rules/jm');
-const { applyPermissions: applyCaPermissions } = require('./rules/ca');
-const { applyPermissions: applyOpenPermissions } = require('./rules/open');
+const { setupCanForRules } = require('./setupCanForRules');
+
+const zmRules = require('./rules/zm.json');
+const openRules = require('./rules/open.json');
+
 const { canEditCase, canViewPostSurvey } = require('./middlewares');
 
-const applyPermissions = {
-  zm: applyZmPermissions,
-  za: applyZaPermissions,
-  et: applyEtPermissions,
-  mw: applyMwPermissions,
-  br: applyBrPermissions,
-  in: applyInPermissions,
-  jm: applyJmPermissions,
-  ca: applyCaPermissions,
+// TODO: maybe factor out so it's easier to build the API that retrieves this to the frontend
+const rulesMap = {
+  zm: zmRules,
+};
+
+/**
+ * @type {{ [key in keyof typeof rulesMap]: ReturnType<typeof setupCanForRules> }}
+ */
+const initializedCanMap = Object.entries(rulesMap).reduce((accum, [key, rules]) => {
+  const can = setupCanForRules(rules);
+  return {
+    ...accum,
+    [key]: can,
+  };
+}, {});
+
+const initializedCanOpenRules = setupCanForRules(openRules);
+
+// TODO: complete rulesMap with this keys
+// const applyPermissions = {
+//   zm: applyZmPermissions,
+//   za: applyZaPermissions,
+//   et: applyEtPermissions,
+//   mw: applyMwPermissions,
+//   br: applyBrPermissions,
+//   in: applyInPermissions,
+//   jm: applyJmPermissions,
+//   ca: applyCaPermissions,
+// };
+
+/**
+ * Applies the permissions if valid.
+ * @param {import('express').Request} req
+ * @param {ReturnType<typeof setupCanForRules>} initializedCan
+ * @param {string} permissionsConfig
+ * @throws Will throw if initializedCan is not a function
+ */
+const applyPermissions = (req, initializedCan, permissionsConfig) => {
+  if (!initializedCan) throw new Error(`Cannot find rules for ${permissionsConfig}`);
+
+  if (typeof initializedCan === 'string')
+    throw new Error(`Error in rules for ${permissionsConfig}. Error: ${initializedCan}`);
+
+  if (typeof initializedCan !== 'function')
+    throw new Error(`Error in rules for ${permissionsConfig}. Error: can is not a function.`);
+
+  req.can = initializedCan;
 };
 
 const setupPermissions = (req, res, next) => {
   if (process.env.NODE_ENV === 'test') {
-    applyOpenPermissions(req);
+    applyPermissions(req, initializedCanOpenRules, 'open rules');
     return next();
   }
 
   const { accountSid } = req;
   const permissionsKey = `PERMISSIONS_${accountSid}`;
-  const permissions = process.env[permissionsKey];
-  if (!applyPermissions[permissions])
-    throw new Error('permissions not provided for the specified accountSid.');
+  const permissionsConfig = process.env[permissionsKey];
+  const initializedCan = initializedCanMap[permissionsConfig];
 
-  applyPermissions[permissions](req);
+  applyPermissions(req, initializedCan, permissionsConfig);
   return next();
 };
 
