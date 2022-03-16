a1dbf4e930c6791f053a3caf983a42c658d49411
Stephen Hand <steveh@techmatters.org>
2022-03-16 21:23:12 +0000

    fix linter

--- hrm-service/src/swagger.js
+++ hrm-service/src/swagger.js
@@ -1,6 +1,7 @@
 const swaggerUi = require('swagger-ui-express');
 const yaml = require('js-yaml');
 const fs = require('fs');
+
 const swaggerDocument = yaml.safeLoad(fs.readFileSync('./swagger.yaml', 'utf8'));
 
 const runWhenNotProduction = app => {
