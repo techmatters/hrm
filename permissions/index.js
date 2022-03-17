b9b6038b3fbd8b518243a5c8c71bf7fe1ae3294d
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-03-17 18:34:22 -0300

    Rework permissions files (#153)
    * Grouped actions by target model
    
    * Reworked setupPermissions to use json rules + added open and zm
    
    * Added actions present in frontend but missing here
    
    * Migrated all permissions files to json format

--- permissions/index.js
+++ permissions/index.js
@@ -1,40 +1,75 @@
 const User = require('./user');
 const { SafeRouter, publicEndpoint } = require('./safe-router');
-const { applyPermissions: applyZmPermissions } = require('./rules/zm');
-const { applyPermissions: applyZaPermissions } = require('./rules/za');
-const { applyPermissions: applyEtPermissions } = require('./rules/et');
-const { applyPermissions: applyMwPermissions } = require('./rules/mw');
-const { applyPermissions: applyBrPermissions } = require('./rules/br');
-const { applyPermissions: applyInPermissions } = require('./rules/in');
-const { applyPermissions: applyJmPermissions } = require('./rules/jm');
-const { applyPermissions: applyCaPermissions } = require('./rules/ca');
-const { applyPermissions: applyOpenPermissions } = require('./rules/open');
+const { setupCanForRules } = require('./setupCanForRules');
+
+const openRules = require('./rules/open.json');
+const brRules = require('./rules/br.json');
+const caRules = require('./rules/ca.json');
+const etRules = require('./rules/et.json');
+const inRules = require('./rules/in.json');
+const jmRules = require('./rules/jm.json');
+const mwRules = require('./rules/mw.json');
+const zaRules = require('./rules/za.json');
+const zmRules = require('./rules/zm.json');
+
 const { canEditCase, canViewPostSurvey } = require('./middlewares');
 
-const applyPermissions = {
-  zm: applyZmPermissions,
-  za: applyZaPermissions,
-  et: applyEtPermissions,
-  mw: applyMwPermissions,
-  br: applyBrPermissions,
-  in: applyInPermissions,
-  jm: applyJmPermissions,
-  ca: applyCaPermissions,
+// TODO: maybe factor out so it's easier to build the API that retrieves this to the frontend
+const rulesMap = {
+  br: brRules,
+  ca: caRules,
+  et: etRules,
+  in: inRules,
+  jm: jmRules,
+  mw: mwRules,
+  za: zaRules,
+  zm: zmRules,
+};
+
+/**
+ * @type {{ [key in keyof typeof rulesMap]: ReturnType<typeof setupCanForRules> }}
+ */
+const initializedCanMap = Object.entries(rulesMap).reduce((accum, [key, rules]) => {
+  const can = setupCanForRules(rules);
+  return {
+    ...accum,
+    [key]: can,
+  };
+}, {});
+
+const initializedCanOpenRules = setupCanForRules(openRules);
+
+/**
+ * Applies the permissions if valid.
+ * @param {import('express').Request} req
+ * @param {ReturnType<typeof setupCanForRules>} initializedCan
+ * @param {string} permissionsConfig
+ * @throws Will throw if initializedCan is not a function
+ */
+const applyPermissions = (req, initializedCan, permissionsConfig) => {
+  if (!initializedCan) throw new Error(`Cannot find rules for ${permissionsConfig}`);
+
+  if (typeof initializedCan === 'string')
+    throw new Error(`Error in rules for ${permissionsConfig}. Error: ${initializedCan}`);
+
+  if (typeof initializedCan !== 'function')
+    throw new Error(`Error in rules for ${permissionsConfig}. Error: can is not a function.`);
+
+  req.can = initializedCan;
 };
 
 const setupPermissions = (req, res, next) => {
-  if (process.env.NODE_ENV === 'test') {
-    applyOpenPermissions(req);
+  if (process.env.USE_OPEN_PERMISSIONS || process.env.RUNNING_TESTS) {
+    applyPermissions(req, initializedCanOpenRules, 'open rules');
     return next();
   }
 
   const { accountSid } = req;
   const permissionsKey = `PERMISSIONS_${accountSid}`;
-  const permissions = process.env[permissionsKey];
-  if (!applyPermissions[permissions])
-    throw new Error('permissions not provided for the specified accountSid.');
+  const permissionsConfig = process.env[permissionsKey];
+  const initializedCan = initializedCanMap[permissionsConfig];
 
-  applyPermissions[permissions](req);
+  applyPermissions(req, initializedCan, permissionsConfig);
   return next();
 };
 
