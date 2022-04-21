a98ae7617112afd334f0556d14dfedbab3fe76ba
mmythily <mythily@techmatters.org>
2022-04-21 13:50:07 -0400

    Added CSAMReport endpoint tests on POST route - WIP

--- hrm-service/service-tests/csam-reports.test.ts
+++ hrm-service/service-tests/csam-reports.test.ts
@@ -45,7 +45,6 @@ expect.extend({
   },
 });
 
-
 /**
  * This interacts with the DB
  */
@@ -56,7 +55,28 @@ const server = app.listen();
 const request = supertest.agent(server);
 
 const { accountSid } = mocks;
-const workerSid = 'worker-sid';
+// const workerSid = 'worker-sid';
+/**
+ * Notes: I think these should belong in the mocks.js. I will move them once I finalize this PR. I understand I have to destroy them, and I was hoping you can point me to the best practise to do this.
+ */
+
+const csamReport1 = {
+  csamReportId: 'csam-report-id',
+  twilioWorkerId: 'worker-sid',
+};
+const csamReport2 = {
+  csamReportId: 'csam-report-id',
+  twilioWorkerId: null,
+};
+const csamReport3 = {
+  csamReportId: null,
+  twilioWorkerId: 'worker-sid',
+};
+const csamReport4 = {
+  csamReportId: 'csam-report-id',
+  twilioWorkerId: 'worker-sid',
+  contactId: 1234,
+};
 
 const headers = {
   'Content-Type': 'application/json',
@@ -87,16 +107,21 @@ afterAll(done => {
 describe('/csamReports route', () => {
   const route = `/v0/accounts/${accountSid}/csamReports`;
 
-  const csamReportId = 'csam-report-id';
+  // const csamReportId = 'csam-report-id';
 
-  const body = {
-    csamReportId,
-    twilioWorkerId: workerSid,
+  const expected = {
+    id: expect.anything(),
+    accountSid: accountSid,
+    csamReportId: 'csam-report-id',
+    twilioWorkerId: 'worker-sid',
+    contactId: null,
+    updatedAt: expect.toParseAsDate(),
+    createdAt: expect.toParseAsDate(),
   };
 
   describe('POST', () => {
     test('should return 401', async () => {
-      const response = await request.post(route).send(body);
+      const response = await request.post(route).send(csamReport1);
 
       expect(response.status).toBe(401);
       expect(response.body.error).toBe('Authorization failed');
@@ -106,14 +131,57 @@ describe('/csamReports route', () => {
       const response = await request
         .post(route)
         .set(headers)
-        .send(body);
+        .send(csamReport1);
 
       expect(response.status).toBe(200);
-      expect(response.body.csamReportId).toEqual(csamReportId);
+      expect(response.body).toStrictEqual(expected);
 
       const reportFromDB = await CSAMReportController.getCSAMReport(response.body.id, accountSid);
+      /**
+       * Notes: console logging reportFromDB is not returning anything in console. I am thinking the 2 statements below are not being executed.
+       *
+       */
       expect(reportFromDB).toBeDefined();
-      expect(reportFromDB.csamReportId).toEqual(csamReportId);
+      expect(reportFromDB.csamReportId).toEqual(expected.csamReportId);
+    });
+
+    test('unsure - when request is missing twilioWorkerId', async () => {
+      const response = await request
+        .post(route)
+        .set(headers)
+        .send(csamReport2);
+      expect(response.status).toBe(200);
+      expect(response.body.twilioWorkerId).toEqual('');
+      /**
+       * Notes: This is just a passing test. I was expecting this test to have a status of 500 and returning an empty response body
+       *
+       */
+    });
+    test('unsure - when request is missing csamReportId', async () => {
+      const response = await request
+        .post(route)
+        .set(headers)
+        .send(csamReport3);
+      expect(response.status).toBe(200);
+      expect(response.body.csamReportId).toEqual('');
+      /**
+       * Notes: This is just a passing test. I was expecting this test to have a status of 500 and returning an empty response body
+       *
+       */
+    });
+    test('unsure - scenario when contactId is part of response', async () => {
+      const response = await request
+        .post(route)
+        .set(headers)
+        .send(csamReport4);
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({});
+      /**
+       * Notes: This is just a passing test. I was expecting this test to have a status of 200 and returning a response body similar to 2nd test in this suite, i.e., similar to expected stated above.
+       *
+       * My reasoning is: contactId has a relation to csamReport. While this is not actually being leveraged by the UI, it is something given the sql data model of CSAMReport, should return the corresponding contactId. I might just be missing something.
+       *
+       */
     });
   });
 });
