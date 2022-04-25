700d635efed83321028bb13168352c7bd0abff95
mmythily <mythily@techmatters.org>
2022-04-25 08:59:04 -0400

    Modified CSAM route tests based on code review and added scenarios based on valid and invalid contactId in csam query

--- hrm-service/service-tests/csam-reports.test.ts
+++ hrm-service/service-tests/csam-reports.test.ts
@@ -3,66 +3,27 @@ const Sequelize = require('sequelize');
 const app = require('../src/app');
 const models = require('../src/models');
 const mocks = require('./mocks');
-
-//Notes: I am having a hard time importing this from `./case-validation.ts`
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toParseAsDate(date: Date): R;
-    }
-    // @ts-ignore
-    interface Expect<R> {
-      toParseAsDate(date: Date): R;
-    }
-  }
-}
-
-expect.extend({
-  toParseAsDate(received, date) {
-    let receivedDate;
-    try {
-      receivedDate = received instanceof Date ? received : Date.parse(received);
-    } catch (e) {
-      return {
-        pass: false,
-        message: () => `Expected '${received}' to be a parseable date. Error: ${e}`,
-      };
-    }
-
-    if (date) {
-      const expectedDate = typeof date === 'string' ? Date.parse(date) : date;
-      const pass = receivedDate.valueOf() === expectedDate.valueOf();
-      return {
-        pass,
-        message: () => `Expected '${received}' to be the same as '${expectedDate.toISOString()}'`,
-      };
-    }
-
-    return {
-      pass: true,
-      message: () => `Expected '${received}' to be a parseable date.`,
-    };
-  },
-});
+import './case-validation';
 
 /**
  * This interacts with the DB
  */
-const { CSAMReport } = models;
+const { CSAMReport, Contact } = models;
 const CSAMReportController = require('../src/controllers/csam-report-controller')(CSAMReport);
 
 const server = app.listen();
 const request = supertest.agent(server);
 
 const { accountSid } = mocks;
-// const workerSid = 'worker-sid';
+const workerSid = 'worker-sid';
 /**
  * Notes: I think these should belong in the mocks.js. I will move them once I finalize this PR. I understand I have to destroy them, and I was hoping you can point me to the best practise to do this.
  */
 
 const csamReport1 = {
   csamReportId: 'csam-report-id',
-  twilioWorkerId: 'worker-sid',
+  twilioWorkerId: workerSid,
+  contactId: null,
 };
 const csamReport2 = {
   csamReportId: 'csam-report-id',
@@ -70,12 +31,14 @@ const csamReport2 = {
 };
 const csamReport3 = {
   csamReportId: null,
-  twilioWorkerId: 'worker-sid',
+  twilioWorkerId: workerSid,
 };
-const csamReport4 = {
+
+const { contact1 } = mocks;
+const invalidContactCsamReport = {
   csamReportId: 'csam-report-id',
   twilioWorkerId: 'worker-sid',
-  contactId: 1234,
+  contactId: 12345,
 };
 
 const headers = {
@@ -91,24 +54,32 @@ const csamReports2DestroyQuery = {
   },
 };
 
+const query = {
+  where: {
+    twilioWorkerId: {
+      [Sequelize.Op.in]: [workerSid],
+    },
+  },
+};
+
 beforeAll(async () => {
   await CSAMReport.destroy(csamReports2DestroyQuery);
+  await Contact.destroy(query);
 });
 
 afterAll(done => {
   server.close(() => {
     CSAMReport.destroy(csamReports2DestroyQuery).then(() => {
-      console.log('csam reports test cleaned up.');
-      done();
+      Contact.destroy(query).then(() => {
+        done();
+      });
     });
   });
+  console.log('csam reports test cleaned up.');
 });
 
 describe('/csamReports route', () => {
   const route = `/v0/accounts/${accountSid}/csamReports`;
-
-  // const csamReportId = 'csam-report-id';
-
   const expected = {
     id: expect.anything(),
     accountSid: accountSid,
@@ -137,51 +108,60 @@ describe('/csamReports route', () => {
       expect(response.body).toStrictEqual(expected);
 
       const reportFromDB = await CSAMReportController.getCSAMReport(response.body.id, accountSid);
-      /**
-       * Notes: console logging reportFromDB is not returning anything in console. I am thinking the 2 statements below are not being executed.
-       *
-       */
       expect(reportFromDB).toBeDefined();
       expect(reportFromDB.csamReportId).toEqual(expected.csamReportId);
     });
 
-    test('unsure - when request is missing twilioWorkerId', async () => {
+    test('valid contactId, should return 200 and update the database correctly', async () => {
+      //Create a Contact for the contactId
+      const contactRoute = `/v0/accounts/${accountSid}/contacts`;
+      const contactResponse = await request
+        .post(contactRoute)
+        .set(headers)
+        .send(contact1);
+      let csamReportWithContactId = { ...csamReport1, contactId: contactResponse.body.id };
+
+      const response = await request
+        .post(route)
+        .set(headers)
+        .send(csamReportWithContactId);
+
+      expect(response.status).toBe(200);
+      expect(response.body.contactId).toBe(csamReportWithContactId.contactId);
+
+      const reportFromDB = await CSAMReportController.getCSAMReport(response.body.id, accountSid);
+      expect(reportFromDB.csamReportId).toEqual(expected.csamReportId);
+    });
+    test('invalid contactId, returns 500', async () => {
+      const response = await request
+        .post(route)
+        .set(headers)
+        .send(invalidContactCsamReport);
+
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({});
+    });
+    test('missing twilioWorkerId, returns 200', async () => {
       const response = await request
         .post(route)
         .set(headers)
         .send(csamReport2);
       expect(response.status).toBe(200);
       expect(response.body.twilioWorkerId).toEqual('');
-      /**
-       * Notes: This is just a passing test. I was expecting this test to have a status of 500 and returning an empty response body
-       *
-       */
+
+      const reportFromDB = await CSAMReportController.getCSAMReport(response.body.id, accountSid);
+      expect(reportFromDB.csamReportId).toEqual(expected.csamReportId);
     });
-    test('unsure - when request is missing csamReportId', async () => {
+    test('missing csamReportId, returns 200', async () => {
       const response = await request
         .post(route)
         .set(headers)
         .send(csamReport3);
       expect(response.status).toBe(200);
+
       expect(response.body.csamReportId).toEqual('');
-      /**
-       * Notes: This is just a passing test. I was expecting this test to have a status of 500 and returning an empty response body
-       *
-       */
-    });
-    test('unsure - scenario when contactId is part of response', async () => {
-      const response = await request
-        .post(route)
-        .set(headers)
-        .send(csamReport4);
-      expect(response.status).toBe(500);
-      expect(response.body).toEqual({});
-      /**
-       * Notes: This is just a passing test. I was expecting this test to have a status of 200 and returning a response body similar to 2nd test in this suite, i.e., similar to expected stated above.
-       *
-       * My reasoning is: contactId has a relation to csamReport. While this is not actually being leveraged by the UI, it is something given the sql data model of CSAMReport, should return the corresponding contactId. I might just be missing something.
-       *
-       */
+      const reportFromDB = await CSAMReportController.getCSAMReport(response.body.id, accountSid);
+      expect(reportFromDB).toBeDefined();
     });
   });
 });
