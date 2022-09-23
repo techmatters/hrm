e7c8395ba4a17cda2d1e4fe915e528c57236e7b2
Robert Bo Davis <bo@interrobang.consulting>
2022-09-23 16:32:34 -0400

    fix: add .gitignore for compiled js

--- lambdas/src/mock/helloWorld.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.helloWorld = void 0;
-var helloWorld = function () {
-    // eslint-disable-next-line no-console
-    console.log("Hello World");
-};
-exports.helloWorld = helloWorld;
--- lambdas/src/mock/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.handler = void 0;
-// eslint-disable-next-line import/no-extraneous-dependencies
-var helloWorld_1 = require("./helloWorld");
-var handler = function () {
-    (0, helloWorld_1.helloWorld)();
-};
-exports.handler = handler;
