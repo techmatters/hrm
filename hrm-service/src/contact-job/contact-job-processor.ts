dbe9dedd6ee7f7a2a8664d0cf78608fd9febd250
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-09-27 16:25:23 -0300

    Added publishing behavior to retrieve contact transcript jobs + moved pulling due jobs to processor

--- hrm-service/src/contact-job/contact-job-processor.ts
+++ hrm-service/src/contact-job/contact-job-processor.ts
@@ -1,11 +1,13 @@
 import { setInterval } from 'timers';
+import { subMilliseconds } from 'date-fns';
+import { pullDueContactJobs } from './contact-job-data-access';
 import { processCompleteContactJobs } from './contact-job-complete';
-import { publishPendingContactJobs } from './contact-job-publish';
+import { publishDueContactJobs } from './contact-job-publish';
 
 let processingJobs = false;
 
-const JOB_PROCESSING_INTERVAL_MILLISECONDS = 5000;
-const JOB_RETRY_INTERVAL_MILLISECONDS = 60000;
+const JOB_PROCESSING_INTERVAL_MILLISECONDS = 5000; // 5 seconds
+const JOB_RETRY_INTERVAL_MILLISECONDS = 120000; // 2 minutes
 
 export const getProcessingInterval = () => JOB_PROCESSING_INTERVAL_MILLISECONDS;
 export const getRetryInterval = () => JOB_RETRY_INTERVAL_MILLISECONDS;
@@ -16,20 +18,16 @@ export function processContactJobs() {
     processingJobs = true;
 
     return setInterval(async () => {
-      // This is the primary job processing 'loop'
-      // First we process any completed jobs. These completed jobs would be pulled from a queue in prod but we just have a cheap in memory simulation using an array here
       try {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const completedJobs = await processCompleteContactJobs();
 
-        // Process completed jobs
-        // console.log(completedJobs);
+        const dueContactJobs = await pullDueContactJobs(
+          subMilliseconds(new Date(), getRetryInterval()),
+        );
 
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const pusblishResults = await publishPendingContactJobs(getRetryInterval());
-
-        // Process published results
-        // console.log(pusblishResults);
+        const publishedContactJobResult = await publishDueContactJobs(dueContactJobs);
       } catch (err) {
         console.error('JOB PROCESSING SWEEP ABORTED DUE TO UNHANDLED ERROR', err);
       }
