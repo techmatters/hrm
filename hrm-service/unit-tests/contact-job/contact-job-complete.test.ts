5ef938e5c6c89369a3749cb29bd6bb26bc2e5ca5
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-09-28 16:21:34 -0300

    Added unit tests to contact-job-complete

---
+++ hrm-service/unit-tests/contact-job/contact-job-complete.test.ts
@@ -0,0 +1,106 @@
+import * as SQSClient from '../../src/contact-job/client-sqs';
+import * as contactJobDataAccess from '../../src/contact-job/contact-job-data-access';
+import * as contactJobComplete from '../../src/contact-job/contact-job-complete';
+import { ContactJobType } from '../../src/contact-job/contact-job-data-access';
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('pollAndprocessCompletedContactJobs', () => {
+  test('Completed jobs are polled as expected', async () => {
+    const sqsSpy = jest
+      .spyOn(SQSClient, 'pollCompletedContactJobs')
+      .mockImplementation(async () => ({
+        Messages: [],
+      }));
+
+    const result = await contactJobComplete.pollAndprocessCompletedContactJobs();
+
+    expect(sqsSpy).toHaveBeenCalled();
+    expect(Array.isArray(result)).toBeTruthy();
+    expect(result.length).toBe(0);
+  });
+
+  test('Invalid job format throws but does not shuts down other jobs', async () => {
+    const invalidPayload = {
+      Body: JSON.stringify({ jobType: 'invalid' }),
+      ReceiptHandle: 'invalid',
+    };
+    const validPayload = {
+      Body: JSON.stringify({
+        jobType: ContactJobType.RETRIEVE_CONTACT_TRANSCRIPT,
+      }),
+      ReceiptHandle: 'valid',
+    };
+
+    jest.spyOn(SQSClient, 'pollCompletedContactJobs').mockImplementation(async () => ({
+      Messages: [invalidPayload, validPayload],
+    }));
+    const errorSpy = jest.spyOn(console, 'error');
+    const processCompletedRetrieveContactTranscriptSpy = jest
+      .spyOn(contactJobComplete, 'processCompletedRetrieveContactTranscript')
+      .mockImplementation(async () => {});
+
+    jest
+      .spyOn(contactJobDataAccess, 'completeContactJob')
+      .mockImplementation(async () => validPayload as any);
+
+    const result = await contactJobComplete.pollAndprocessCompletedContactJobs();
+
+    expect(processCompletedRetrieveContactTranscriptSpy).toHaveBeenCalledTimes(1);
+    expect(errorSpy).toHaveBeenCalledTimes(1);
+    expect(errorSpy).toHaveBeenCalledWith(
+      'Failed to process CompletedContactJobBody:',
+      invalidPayload,
+      new Error(`Unhandled case: ${invalidPayload}`),
+    );
+
+    expect(result[0].status).toBe('rejected');
+    expect(result[1].status).toBe('fulfilled');
+
+    console.log(result);
+  });
+
+  test('RETRIEVE_CONTACT_TRANSCRIPT job is processed accordingly', async () => {
+    const job = {
+      jobType: ContactJobType.RETRIEVE_CONTACT_TRANSCRIPT,
+      jobId: 1,
+      accountSid: 'accountSid',
+      contactId: 'contactId',
+      taskId: 'taskId',
+      twilioWorkerId: 'twilioWorkerId',
+      serviceSid: 'serviceSid',
+      channelSid: 'channelSid',
+      filePath: 'filePath',
+      completionPayload: 'completionPayload',
+    };
+    const validPayload = {
+      Body: JSON.stringify(job),
+      ReceiptHandle: 'valid',
+    };
+
+    jest.spyOn(SQSClient, 'pollCompletedContactJobs').mockImplementation(async () => ({
+      Messages: [validPayload],
+    }));
+    const deletedCompletedContactJobsSpy = jest
+      .spyOn(SQSClient, 'deletedCompletedContactJobs')
+      .mockImplementation(async () => {});
+    const processCompletedRetrieveContactTranscriptSpy = jest
+      .spyOn(contactJobComplete, 'processCompletedRetrieveContactTranscript')
+      .mockImplementation(async () => {});
+    const completeContactJobSpy = jest
+      .spyOn(contactJobDataAccess, 'completeContactJob')
+      .mockImplementation(async () => validPayload as any);
+
+    const result = await contactJobComplete.pollAndprocessCompletedContactJobs();
+
+    expect(processCompletedRetrieveContactTranscriptSpy).toHaveBeenCalledWith(job);
+    expect(completeContactJobSpy).toHaveBeenCalledWith(job.jobId, job.completionPayload);
+    expect(deletedCompletedContactJobsSpy).toHaveBeenCalledWith(validPayload.ReceiptHandle);
+
+    expect(result[0].status).toBe('fulfilled');
+
+    console.log(result);
+  });
+});
