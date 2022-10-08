db261b5696bbe3c3e7eb533667ca5844e209b7cc
Robert Bo Davis <bo@interrobang.consulting>
2022-10-08 11:21:42 -0400

    fix: missing test js files

---
+++ lambdas/src/contact-retrieve-recording-url/tests/generateMockMessageBody.js
@@ -0,0 +1,12 @@
+const accountSids = ['testSid1', 'testSid2'];
+
+export const generateMessage = () => {
+  const accountSid = accountSids[Math.floor(Math.random() * accountSids.length)];
+  return {
+    jobId: Math.floor(Math.random() * 1000000),
+    accountSid,
+    contactId: Math.floor(Math.random() * 1000000),
+    jobType: 'retrieve-contact-recording-url',
+    filePath: `accountSid/testFilePath-${Math.floor(Math.random() * 1000000)}`,
+  };
+};
