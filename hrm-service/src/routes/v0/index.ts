667ae1d0de7588cf385a68c215df720f077b3145
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-11-16 20:43:11 -0300

    Replaced the csam v0 router

--- hrm-service/src/routes/v0/index.ts
+++ hrm-service/src/routes/v0/index.ts
@@ -1,12 +1,12 @@
+import { IRouter, Router } from 'express';
+
 import cases from '../../case/case-routes-v0';
 import contacts from '../../contact/contact-routes-v0';
+import csamReports from '../../csam-report/csam-report-routes-v0';
 import permissions from '../../permissions/permissions-routes-v0';
-import { IRouter } from 'express';
 import { Permissions } from '../../permissions';
 
-const { Router } = require('express');
 const postSurveys = require('./post-surveys');
-const csamReports = require('./csam-reports');
 
 export default (rules: Permissions) => {
   const router: IRouter = Router();
