4d03eaf8829d302ff283e3e52e545dc6688d3f11
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2022-11-24 11:58:49 -0300

    Service tests (csam)

--- hrm-service/service-tests/csam-reports.test.ts
+++ hrm-service/service-tests/csam-reports.test.ts
@@ -1,4 +1,5 @@
 import supertest from 'supertest';
+import each from 'jest-each';
 import { createService } from '../src/app';
 import * as mocks from './mocks';
 import './case-validation';
@@ -18,26 +19,13 @@ const request = supertest.agent(server);
 
 const { accountSid, workerSid } = mocks;
 
-const csamReport1 = {
+const csamReport1: Partial<csamReportsApi.CreateCSAMReport> = {
   csamReportId: 'csam-report-id',
   twilioWorkerId: workerSid,
-  contactId: null,
-};
-const csamReport2 = {
-  csamReportId: 'csam-report-id',
-  twilioWorkerId: null,
-};
-const csamReport3 = {
-  csamReportId: null,
-  twilioWorkerId: workerSid,
+  contactId: undefined,
 };
 
 const { contact1 } = mocks;
-const invalidContactCsamReport = {
-  csamReportId: 'csam-report-id',
-  twilioWorkerId: workerSid,
-  contactId: 1234,
-};
 
 const headers = {
   'Content-Type': 'application/json',
@@ -69,16 +57,6 @@ afterAll(async () => {
 
 describe('/csamReports route', () => {
   const route = `/v0/accounts/${accountSid}/csamReports`;
-  const expected = {
-    id: expect.anything(),
-    accountSid: accountSid,
-    csamReportId: 'csam-report-id',
-    twilioWorkerId: workerSid,
-    contactId: null,
-    updatedAt: expect.toParseAsDate(),
-    createdAt: expect.toParseAsDate(),
-  };
-
   describe('POST', () => {
     test('should return 401', async () => {
       const response = await request.post(route).send(csamReport1);
@@ -87,109 +65,204 @@ describe('/csamReports route', () => {
       expect(response.body.error).toBe('Authorization failed');
     });
 
-    test('should return 200', async () => {
-      const response = await request
-        .post(route)
-        .set(headers)
-        .send(csamReport1);
+    describe('Should return 422', () => {
+      const testCases: {
+        description: string;
+        csamReport: Partial<csamReportsApi.CreateCSAMReport>;
+      }[] = [
+        {
+          description: 'when reportType is defined but invalid',
+          csamReport: {
+            csamReportId: 'csam-report-id',
+            twilioWorkerId: workerSid,
+            reportType: 'invalid' as any,
+          },
+        },
+        {
+          description: 'when reportType is undefined and no csamReportId is provided',
+          csamReport: {
+            twilioWorkerId: workerSid,
+          },
+        },
+        {
+          description: 'when reportType is "counsellor-generated" and no csamReportId is provided',
+          csamReport: {
+            twilioWorkerId: workerSid,
+            reportType: 'counsellor-generated',
+          },
+        },
+      ];
 
-      expect(response.status).toBe(200);
-      expect(response.body).toStrictEqual(expected);
+      each(testCases).test('$description', async ({ csamReport }) => {
+        const response = await request
+          .post(route)
+          .set(headers)
+          .send(csamReport);
 
-      const reportFromDB = await csamReportsApi.getCSAMReport(response.body.id, accountSid);
-
-      if (!reportFromDB) {
-        throw new Error('reportFromDB is undefined');
-      }
-
-      expect(reportFromDB.csamReportId).toEqual(expected.csamReportId);
+        expect(response.status).toBe(422);
+      });
     });
 
-    test('valid contactId, should return 200 and update the database correctly', async () => {
-      //Create a Contact for the contactId
-      const contactRoute = `/v0/accounts/${accountSid}/contacts`;
-      const contactResponse = await request
-        .post(contactRoute)
-        .set(headers)
-        .send(contact1);
-      let csamReportWithContactId = { ...csamReport1, contactId: contactResponse.body.id };
+    describe('Should return 200', () => {
+      describe('with valid arguments', () => {
+        const testCasesWithContact: {
+          description: string;
+          csamReport: Partial<csamReportsApi.CreateCSAMReport>;
+          contact?: any;
+        }[] = [
+          // with contact
+          {
+            description: 'when reportType is "counsellor-generated", twilioWorkerId preset',
+            csamReport: {
+              twilioWorkerId: workerSid,
+              reportType: 'counsellor-generated',
+              csamReportId: 'csam-report-id',
+            },
+            contact: contact1,
+          },
+          {
+            description: 'when reportType is "self-generated", twilioWorkerId preset',
+            csamReport: {
+              twilioWorkerId: workerSid,
+              reportType: 'self-generated',
+            },
+            contact: contact1,
+          },
+          {
+            description: 'when reportType is "counsellor-generated", twilioWorkerId absent',
+            csamReport: {
+              reportType: 'counsellor-generated',
+              csamReportId: 'csam-report-id',
+            },
+          },
+          {
+            description: 'when reportType is "self-generated", twilioWorkerId absent',
+            csamReport: {
+              reportType: 'self-generated',
+            },
+          },
+        ];
 
-      const response = await request
-        .post(route)
-        .set(headers)
-        .send(csamReportWithContactId);
+        const testCases = testCasesWithContact.flatMap(({ contact, description, ...rest }) => [
+          { ...rest, description: description + ' without contact' },
+          { ...rest, contact, description: description + ' with contact' },
+        ]);
 
-      expect(response.status).toBe(200);
-      expect(response.body.contactId).toBe(csamReportWithContactId.contactId);
+        each(testCases).test('$description', async ({ csamReport, contact }) => {
+          let csamReportToSave;
 
-      const reportFromDB = await csamReportsApi.getCSAMReport(response.body.id, accountSid);
+          if (contact) {
+            //Create a Contact for the contactId
+            const contactRoute = `/v0/accounts/${accountSid}/contacts`;
+            const contactResponse = await request
+              .post(contactRoute)
+              .set(headers)
+              .send(contact);
 
-      if (!reportFromDB) {
-        throw new Error('reportFromDB is undefined');
-      }
+            csamReportToSave = { ...csamReport, contactId: contactResponse.body.id };
+          } else {
+            csamReportToSave = { ...csamReport };
+          }
 
-      expect(reportFromDB.csamReportId).toEqual(expected.csamReportId);
+          const expected = {
+            ...csamReportToSave,
+            id: expect.anything(),
+            accountSid: accountSid,
+            updatedAt: expect.toParseAsDate(),
+            createdAt: expect.toParseAsDate(),
+          };
+
+          const response = await request
+            .post(route)
+            .set(headers)
+            .send(csamReportToSave);
+
+          expect(response.status).toBe(200);
+          if (contact) {
+            expect(response.body.contactId).toBe(csamReportToSave.contactId);
+          } else {
+            expect(response.body.contactId).toBeNull();
+          }
+
+          const reportFromDB = await csamReportsApi.getCSAMReport(response.body.id, accountSid);
+
+          if (!reportFromDB) {
+            throw new Error('reportFromDB is undefined');
+          }
+
+          expect(reportFromDB).toEqual(expect.objectContaining(expected));
+
+          if (csamReport.reportType === 'counsellor-generated') {
+            expect(reportFromDB.csamReportId).toEqual(csamReport.csamReportId);
+          } else {
+            expect(reportFromDB.csamReportId).toBeDefined();
+          }
+
+          if (csamReport.twilioWorkerId) {
+            expect(reportFromDB.twilioWorkerId).toBe(csamReport.twilioWorkerId);
+          } else {
+            expect(reportFromDB.twilioWorkerId).toBe('');
+          }
+        });
+      });
     });
 
-    test('invalid contactId, returns 500', async () => {
-      const response = await request
-        .post(route)
-        .set(headers)
-        .send(invalidContactCsamReport);
+    describe('Should return 500', () => {
+      const testCases: {
+        description: string;
+        csamReport: Partial<csamReportsApi.CreateCSAMReport>;
+      }[] = [
+        {
+          description: 'when reportType is "counsellor-generated"',
+          csamReport: {
+            twilioWorkerId: workerSid,
+            reportType: 'counsellor-generated',
+            csamReportId: 'csam-report-id',
+            contactId: 99999999,
+          },
+        },
+        {
+          description: 'when reportType is "self-generated"',
+          csamReport: {
+            twilioWorkerId: workerSid,
+            reportType: 'self-generated',
+            csamReportId: 'csam-report-id',
+            contactId: 99999999,
+          },
+        },
+      ];
 
-      expect(response.status).toBe(500);
-      expect(response.body.message).toContain(
-        'insert or update on table "CSAMReports" violates foreign key constraint "CSAMReports_contactId_accountSid_fkey"',
-      );
-    });
+      each(testCases).test('Invalid contactId, $description', async ({ csamReport }) => {
+        const response = await request
+          .post(route)
+          .set(headers)
+          .send(csamReport);
 
-    test('invalid accountSid, returns 500', async () => {
-      //Create a Contact for the contactId
-      const contactRoute = `/v0/accounts/${accountSid}/contacts`;
-      const contactResponse = await request
-        .post(contactRoute)
-        .set(headers)
-        .send(contact1);
-      let csamReportWithContactId = { ...csamReport1, contactId: contactResponse.body.id };
+        expect(response.status).toBe(500);
+        expect(response.body.message).toContain(
+          'insert or update on table "CSAMReports" violates foreign key constraint "CSAMReports_contactId_accountSid_fkey"',
+        );
+      });
 
-      const response = await request
-        .post(route.replace(accountSid, 'another-account-sid'))
-        .set(headers)
-        .send(csamReportWithContactId);
+      each(testCases).test('Invalid accountSid, $description', async ({ csamReport }) => {
+        //Create a Contact for the contactId
+        const contactRoute = `/v0/accounts/${accountSid}/contacts`;
+        const contactResponse = await request
+          .post(contactRoute)
+          .set(headers)
+          .send(contact1);
+        let csamReportWithContactId = { ...csamReport, contactId: contactResponse.body.id };
 
-      expect(response.status).toBe(500);
-      expect(response.body.message).toContain(
-        'insert or update on table "CSAMReports" violates foreign key constraint "CSAMReports_contactId_accountSid_fkey"',
-      );
-    });
+        const response = await request
+          .post(route.replace(accountSid, 'another-account-sid'))
+          .set(headers)
+          .send(csamReportWithContactId);
 
-    test('missing twilioWorkerId, returns 200', async () => {
-      const response = await request
-        .post(route)
-        .set(headers)
-        .send(csamReport2);
-      expect(response.status).toBe(200);
-      expect(response.body.twilioWorkerId).toEqual('');
-
-      const reportFromDB = await csamReportsApi.getCSAMReport(response.body.id, accountSid);
-
-      if (!reportFromDB) {
-        throw new Error('reportFromDB is undefined');
-      }
-
-      expect(reportFromDB.csamReportId).toEqual(expected.csamReportId);
-    });
-
-    test('missing csamReportId, returns 200', async () => {
-      const response = await request
-        .post(route)
-        .set(headers)
-        .send(csamReport3);
-      expect(response.status).toBe(200);
-
-      expect(response.body.csamReportId).toEqual('');
-      const reportFromDB = await csamReportsApi.getCSAMReport(response.body.id, accountSid);
-      expect(reportFromDB).toBeDefined();
+        expect(response.status).toBe(500);
+        expect(response.body.message).toContain(
+          'insert or update on table "CSAMReports" violates foreign key constraint "CSAMReports_contactId_accountSid_fkey"',
+        );
+      });
     });
   });
 });
