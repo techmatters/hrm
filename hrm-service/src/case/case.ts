7ba839497cd3e84502b9f87f6c656e128e9e07b0
Robert Bo Davis <bo@interrobang.consulting>
2023-02-01 20:04:05 -0500

    CHI-1654 license header (#292)
    * feat: license header and ci check
    
    * fix: no terminal in actions
    
    * fix: whitespace nit
    
    * chore: update license check step name in action
    
    * chore update license in package.json
    
    * chore: a bit of cleanup
    
    * chore: run license add again

--- hrm-service/src/case/case.ts
+++ hrm-service/src/case/case.ts
@@ -1,4 +1,20 @@
 /**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+/**
  * This is the 'business logic' module for Case CRUD operations.
  * For the moment it just does some light mapping between the types used for the REST layer, and the types used for the database layer.
  * This includes compatibility code required to provide cases in a shape expected by older clients
