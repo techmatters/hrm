34a50d2ff2834475319f3f3b4f3f8e839f7746e6
Robert Bo Davis <bo@interrobang.consulting>
2023-03-13 15:24:21 -0400

    fix: twilio-client returns wrong client when getClient() called with two account configs. (#319)
    * fix: twilio-client returning single client when a client for a different account SID requested
    
    * test: add tests
    
    * chore: let should be const

--- packages/twilio-client/mockClient/getMockClient.ts
+++ packages/twilio-client/mockClient/getMockClient.ts
@@ -17,8 +17,13 @@
 import RestException from 'twilio/lib/base/RestException';
 import { chatMessageList } from './chatMessageList';
 
-export const getMockClient = () => {
+export const getMockClient = ({ accountSid }: { accountSid: string }) => {
   return {
+    tokens: {
+      toJSON: () => ({
+        accountSid,
+      }),
+    },
     chat: {
       v2: {
         services: () => ({
