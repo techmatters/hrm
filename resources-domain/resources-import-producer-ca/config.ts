d96adb1ec70de7f968d90e8e3226f4844a30f6b9
Stephen Hand <steveh@techmatters.org>
2023-05-09 15:42:02 +0100

    Licence header

--- resources-domain/resources-import-producer-ca/config.ts
+++ resources-domain/resources-import-producer-ca/config.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
 // eslint-disable-next-line prettier/prettier
 import type { AccountSID } from '@tech-matters/twilio-worker-auth';
 import { getSsmParameter } from '@tech-matters/hrm-ssm-cache';
@@ -8,7 +24,7 @@ const getConfig = async () => {
     throw new Error('Missing NODE_ENV');
   }
   const helplineShortCode = process.env.helpline_short_code ?? 'as';
-  
+
   const accountSid: AccountSID = (await getSsmParameter(
     `/${deploymentEnvironment}/twilio/${helplineShortCode.toUpperCase()}/account_sid`,
   )) as AccountSID;
