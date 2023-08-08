730190810c36f0cf591653afcad045691dca9bfa
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2023-08-08 16:46:22 -0300

    Lint

--- hrm-domain/hrm-service/src/case/sql/case-search-sql.ts
+++ hrm-domain/hrm-service/src/case/sql/case-search-sql.ts
@@ -28,7 +28,8 @@ export const OrderByDirection = {
   descending: 'DESC',
 } as const;
 
-export type OrderByDirectionType = typeof OrderByDirection[keyof typeof OrderByDirection];
+export type OrderByDirectionType =
+  (typeof OrderByDirection)[keyof typeof OrderByDirection];
 
 export const OrderByColumn = {
   ID: 'id',
@@ -38,7 +39,7 @@ export const OrderByColumn = {
   FOLLOW_UP_DATE: 'info.followUpDate',
 } as const;
 
-export type OrderByColumnType = typeof OrderByColumn[keyof typeof OrderByColumn];
+export type OrderByColumnType = (typeof OrderByColumn)[keyof typeof OrderByColumn];
 
 const ORDER_BY_FIELDS: Record<OrderByColumnType, string> = {
   id: pgp.as.name('id'),
@@ -144,7 +145,11 @@ const filterSql = ({
     ...[
       dateFilterCondition(FilterableDateField.CREATED_AT, 'createdAt', createdAt),
       dateFilterCondition(FilterableDateField.UPDATED_AT, 'updatedAt', updatedAt),
-      dateFilterCondition(FilterableDateField.FOLLOW_UP_DATE, 'followUpDate', followUpDate),
+      dateFilterCondition(
+        FilterableDateField.FOLLOW_UP_DATE,
+        'followUpDate',
+        followUpDate,
+      ),
     ].filter(sql => sql),
   );
   if (categories && categories.length) {
@@ -267,7 +272,10 @@ const selectCasesPaginatedSql = (
   orderByClause: string,
   havingClause: string = '',
 ) => `
-SELECT * FROM (${selectCasesUnorderedSql(whereClause, havingClause)}) "unordered" ${orderByClause}
+SELECT * FROM (${selectCasesUnorderedSql(
+  whereClause,
+  havingClause,
+)}) "unordered" ${orderByClause}
 LIMIT $<limit>
 OFFSET $<offset>`;
 
