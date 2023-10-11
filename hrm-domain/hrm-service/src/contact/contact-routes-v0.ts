4f1d6cb2c19d81ac1500d70eb3a81e9e152d887c
Stephen Hand <steveh@techmatters.org>
2023-10-11 19:02:33 +0100

    Fix twilio login for HRM permission check

--- hrm-domain/hrm-service/src/contact/contact-routes-v0.ts
+++ hrm-domain/hrm-service/src/contact/contact-routes-v0.ts
@@ -146,7 +146,10 @@ const canEditContact = asyncHandler(async (req, res, next) => {
           // It the contact record doesn't show this user as the contact owner, but Twilio shows that they are having the associated task transferred to them, permit the edit
           // Long term it's wrong for HRM to be verifying this itself - we should probably initiate updates for the contact record from the backend transfer logic rather than Flex in future.
           // Then HRM just needs to validate the request is coming from a valid backend service with permission to make the edit.
-          const twilioClient = await getClient({ accountSid });
+          const twilioClient = await getClient({
+            accountSid,
+            authToken: process.env[`TWILIO_AUTH_TOKEN_${accountSid}`],
+          });
           if (
             await isTwilioTaskTransferTarget(
               twilioClient,
