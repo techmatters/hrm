cd151a5615878b89069512658eb787b1c6a22e5d
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2023-10-27 14:44:34 -0300

    Service tests

--- hrm-domain/hrm-service/service-tests/profiles.test.ts
+++ hrm-domain/hrm-service/service-tests/profiles.test.ts
@@ -14,6 +14,7 @@
  * along with this program.  If not, see https://www.gnu.org/licenses/.
  */
 
+import each from 'jest-each';
 import './case-validation';
 import { db } from '../src/connection-pool';
 import * as caseApi from '../src/case/case';
@@ -23,7 +24,6 @@ import { getRequest, getServer, headers, useOpenRules } from './server';
 import * as mocks from './mocks';
 import { mockSuccessfulTwilioAuthentication, mockingProxy } from '@tech-matters/testing';
 import { getOrCreateProfileWithIdentifier } from '../src/profile/profile';
-import { isErr } from '@tech-matters/types';
 import { IdentifierWithProfiles } from '../src/profile/profile-data-access';
 import { twilioUser } from '@tech-matters/twilio-worker-auth';
 
@@ -57,12 +57,10 @@ beforeEach(async () => {
 
 describe('/profiles route', () => {
   const baseRoute = `/v0/accounts/${accountSid}/profiles`;
-
   const identifier = 'identifier';
 
   describe('/identifier/:identifier', () => {
     const buildRoute = (id: string) => `${baseRoute}/identifier/${id}`;
-
     const accounts = [accountSid, 'ANOTHER_ACCOUNT'];
 
     let createdProfiles: { [acc: string]: IdentifierWithProfiles };
@@ -75,15 +73,8 @@ describe('/profiles route', () => {
           accounts.map(acc => getOrCreateProfileWithIdentifier()(identifier, acc)),
         )
       )
-        .map(result => {
-          if (isErr(result)) {
-            throw new Error('Creating profile returned an error');
-          } else {
-            return result.data;
-          }
-        })
+        .map(result => result.unwrap())
         .reduce((accum, curr) => ({ ...accum, [curr.accountSid]: curr }), {});
-
       // Create one case for each
       createdCases = (
         await Promise.all(accounts.map(acc => caseApi.createCase(case1, acc, workerSid)))
@@ -171,90 +162,16 @@ describe('/profiles route', () => {
   });
 
   describe('/:profileId', () => {
-    const buildRoute = (id: number, subroute: 'contacts' | 'cases') =>
-      `${baseRoute}/${id}/${subroute}`;
-
-    const sortById = (a: { id: number }, b: { id: number }) => a.id - b.id;
-
     let createdProfile: IdentifierWithProfiles;
-    let createdCases: caseApi.Case[];
-    let createdContacts: Awaited<ReturnType<typeof contactApi.createContact>>[];
     beforeAll(async () => {
       // Create an identifier
       createdProfile = await getOrCreateProfileWithIdentifier()(
         identifier,
         accountSid,
-      ).then(result => {
-        if (isErr(result)) {
-          throw new Error('Creating profile returned an error');
-        }
-        return result.data;
-      });
-
-      // Create two cases
-      createdCases = await Promise.all(
-        [1, 2].map(() => caseApi.createCase(case1, accountSid, workerSid)),
-      );
-
-      // Create two contacts for each
-      createdContacts = await Promise.all(
-        createdCases.flatMap(createdCase =>
-          [1, 2].map(n =>
-            contactApi
-              .createContact(
-                createdCase.accountSid,
-                workerSid,
-                true,
-                {
-                  ...contact1,
-                  number: identifier,
-                  taskId: contact1.taskId + createdCase.id + n,
-                  profileId: createdProfile.profiles[0].id,
-                  identifierId: createdProfile.id,
-                },
-                {
-                  user: twilioUser(workerSid, []),
-                  can: () => true,
-                },
-              )
-              .then(contact =>
-                // Associate contact to case
-                contactApi.connectContactToCase(
-                  contact.accountSid,
-                  workerSid,
-                  String(contact.id),
-                  String(createdCase.id),
-                  {
-                    user: twilioUser(workerSid, []),
-                    can: () => true,
-                  },
-                ),
-              ),
-          ),
-        ),
-      );
-
-      createdCases = await Promise.all(
-        createdCases.map(c =>
-          caseApi.getCase(c.id, c.accountSid, {
-            user: twilioUser(workerSid, []),
-            can: () => true,
-          }),
-        ) as any,
-      );
+      ).then(result => result.unwrap());
     });
 
     afterAll(async () => {
-      await Promise.all(
-        Object.entries(createdContacts).map(([, c]) =>
-          deleteFromTableById('Contacts')(c.id, c.accountSid),
-        ),
-      );
-      await Promise.all(
-        Object.entries(createdCases).map(([, c]) =>
-          deleteFromTableById('Cases')(c.id, c.accountSid),
-        ),
-      );
       await Promise.all([
         ...createdProfile.profiles.map(p =>
           deleteFromTableById('Profiles')(p.id, p.accountSid),
@@ -263,77 +180,317 @@ describe('/profiles route', () => {
       ]);
     });
 
-    const convertContactToExpect = (
-      contact: contactApi.WithLegacyCategories<contactApi.Contact>,
-    ) => ({
-      ...contact,
-      createdAt: expect.toParseAsDate(),
-      updatedAt: expect.toParseAsDate(),
-      finalizedAt: expect.toParseAsDate(),
-      timeOfContact: expect.toParseAsDate(),
-      totalCount: expect.anything(),
-      id: expect.anything(),
-    });
+    describe('GET', () => {
+      const buildRoute = (id: number) => `${baseRoute}/${id}`;
 
-    describe('/contacts', () => {
-      // test('when identifier not exists, return 404', async () => {
-      //   const response = await request.get(buildRoute('not-exists')).set(headers);
-      //   expect(response.statusCode).toBe(404);
-      // });
+      test('when profile not exists, return 404', async () => {
+        const response = await request.get(buildRoute(0)).set(headers);
 
-      test('when profile exists, return 200', async () => {
+        expect(response.statusCode).toBe(404);
+      });
+
+      test('when profile exists, return 200 with profile data', async () => {
         const response = await request
-          .get(buildRoute(createdProfile.profiles[0].id, 'contacts'))
+          .get(buildRoute(createdProfile.profiles[0].id))
           .set(headers);
+
         expect(response.statusCode).toBe(200);
-        expect(response.body.count).toBe(createdContacts.length);
-        expect(response.body.contacts.sort(sortById)).toEqual(
-          expect.objectContaining(
-            createdContacts.sort(sortById).map(convertContactToExpect),
-          ),
-        );
+        console.log(response.body);
+        expect(response.body).toMatchObject(createdProfile.profiles[0]);
       });
     });
 
-    describe('/cases', () => {
-      // test('when identifier not exists, return 404', async () => {
-      //   const response = await request.get(buildRoute('not-exists')).set(headers);
-      //   expect(response.statusCode).toBe(404);
-      // });
+    describe('', () => {
+      const buildRoute = (id: number, subroute: 'contacts' | 'cases') =>
+        `${baseRoute}/${id}/${subroute}`;
 
-      test('when profile exists, return 200', async () => {
-        const response = await request
-          .get(buildRoute(createdProfile.profiles[0].id, 'cases'))
-          .set(headers);
-        expect(response.statusCode).toBe(200);
-        expect(response.body.count).toBe(createdCases.length);
-        expect(
-          response.body.cases
-            .map(({ createdAt, updatedAt, childName, totalCount, ...rest }) => ({
-              ...rest,
-              connectedContacts: rest.connectedContacts?.sort(sortById),
-            }))
-            .sort(sortById),
-        ).toStrictEqual(
-          createdCases
-            .map(({ createdAt, updatedAt, childName, ...rest }) => ({
-              ...rest,
-              connectedContacts: rest.connectedContacts?.sort(sortById),
-            }))
-            .sort(sortById),
+      const sortById = (a: { id: number }, b: { id: number }) => a.id - b.id;
+
+      let createdCases: caseApi.Case[];
+      let createdContacts: Awaited<ReturnType<typeof contactApi.createContact>>[];
+      beforeAll(async () => {
+        // Create two cases
+        createdCases = await Promise.all(
+          [1, 2].map(() => caseApi.createCase(case1, accountSid, workerSid)),
+        );
+
+        // Create two contacts for each
+        createdContacts = await Promise.all(
+          createdCases.flatMap(createdCase =>
+            [1, 2].map(n =>
+              contactApi
+                .createContact(
+                  createdCase.accountSid,
+                  workerSid,
+                  true,
+                  {
+                    ...contact1,
+                    number: identifier,
+                    taskId: contact1.taskId + createdCase.id + n,
+                    profileId: createdProfile.profiles[0].id,
+                    identifierId: createdProfile.id,
+                  },
+                  {
+                    user: twilioUser(workerSid, []),
+                    can: () => true,
+                  },
+                )
+                .then(contact =>
+                  // Associate contact to case
+                  contactApi.connectContactToCase(
+                    contact.accountSid,
+                    workerSid,
+                    String(contact.id),
+                    String(createdCase.id),
+                    {
+                      user: twilioUser(workerSid, []),
+                      can: () => true,
+                    },
+                  ),
+                ),
+            ),
+          ),
+        );
+
+        createdCases = await Promise.all(
+          createdCases.map(c =>
+            caseApi.getCase(c.id, c.accountSid, {
+              user: twilioUser(workerSid, []),
+              can: () => true,
+            }),
+          ) as any,
+        );
+      });
+
+      afterAll(async () => {
+        await Promise.all(
+          Object.entries(createdContacts).map(([, c]) =>
+            deleteFromTableById('Contacts')(c.id, c.accountSid),
+          ),
+        );
+        await Promise.all(
+          Object.entries(createdCases).map(([, c]) =>
+            deleteFromTableById('Cases')(c.id, c.accountSid),
+          ),
+        );
+      });
+
+      const convertContactToExpect = (
+        contact: contactApi.WithLegacyCategories<contactApi.Contact>,
+      ) => ({
+        ...contact,
+        createdAt: expect.toParseAsDate(),
+        updatedAt: expect.toParseAsDate(),
+        finalizedAt: expect.toParseAsDate(),
+        timeOfContact: expect.toParseAsDate(),
+        totalCount: expect.anything(),
+        id: expect.anything(),
+      });
+
+      describe('/contacts', () => {
+        describe('GET', () => {
+          // test('when identifier not exists, return 404', async () => {
+          //   const response = await request.get(buildRoute('not-exists')).set(headers);
+          //   expect(response.statusCode).toBe(404);
+          // });
+
+          test('when profile exists, return 200 with associated contacts', async () => {
+            const response = await request
+              .get(buildRoute(createdProfile.profiles[0].id, 'contacts'))
+              .set(headers);
+
+            expect(response.statusCode).toBe(200);
+            expect(response.body.count).toBe(createdContacts.length);
+            expect(response.body.contacts.sort(sortById)).toEqual(
+              expect.objectContaining(
+                createdContacts.sort(sortById).map(convertContactToExpect),
+              ),
+            );
+          });
+        });
+      });
+
+      describe('/cases', () => {
+        describe('GET', () => {
+          // test('when identifier not exists, return 404', async () => {
+          //   const response = await request.get(buildRoute('not-exists')).set(headers);
+          //   expect(response.statusCode).toBe(404);
+          // });
+
+          test('when profile exists, return 200 with associated cases', async () => {
+            const response = await request
+              .get(buildRoute(createdProfile.profiles[0].id, 'cases'))
+              .set(headers);
+
+            expect(response.statusCode).toBe(200);
+            expect(response.body.count).toBe(createdCases.length);
+            expect(
+              response.body.cases
+                .map(({ createdAt, updatedAt, childName, totalCount, ...rest }) => ({
+                  ...rest,
+                  connectedContacts: rest.connectedContacts?.sort(sortById),
+                }))
+                .sort(sortById),
+            ).toStrictEqual(
+              createdCases
+                .map(({ createdAt, updatedAt, childName, ...rest }) => ({
+                  ...rest,
+                  connectedContacts: rest.connectedContacts?.sort(sortById),
+                }))
+                .sort(sortById),
+            );
+          });
+        });
+      });
+    });
+
+    describe('/:profileFlagId', () => {
+      const buildRoute = (profileId: number, profileFlagId: number) =>
+        `${baseRoute}/${profileId}/flags/${profileFlagId}`;
+
+      let defaultFlags: profilesDB.ProfileFlag[];
+      beforeAll(async () => {
+        defaultFlags = await profilesDB
+          .getProfileFlagsForAccount(accountSid)
+          .then(result => result.unwrap());
+      });
+
+      describe('POST', () => {
+        each([
+          {
+            description: 'auth is missing',
+            expectStatus: 401,
+            customHeaders: {},
+          },
+          {
+            description: 'profile does not exists',
+            profileId: 0,
+            expectStatus: 500,
+          },
+          {
+            description: 'flag does not exists',
+            profileFlagId: 0,
+            expectStatus: 500,
+          },
+          {
+            description: 'profile and flag exist',
+            expectStatus: 200,
+            expectFunction: (response, profileId, profileFlagId) => {
+              expect(response.body.id).toBe(profileId);
+              expect(response.body.profileFlags).toContain(profileFlagId);
+            },
+          },
+        ]).test(
+          'when $description, returns $expectStatus',
+          async ({
+            profileId = createdProfile.profiles[0].id,
+            profileFlagId = defaultFlags[0].id,
+            expectStatus,
+            customHeaders,
+            expectFunction,
+          }) => {
+            const response = await request
+              .post(buildRoute(profileId, profileFlagId))
+              .set(customHeaders || headers);
+            expect(response.statusCode).toBe(expectStatus);
+            console.log(response.body);
+            if (expectFunction) {
+              expectFunction(response, profileId, profileFlagId);
+            }
+
+            // Dissasociate
+            db.task(t =>
+              t.none(
+                `DELETE FROM "ProfilesToProfileFlags" WHERE "profileId" = ${profileId}`,
+              ),
+            );
+          },
+        );
+      });
+
+      describe('DELETE', () => {
+        beforeAll(async () => {
+          (
+            await profilesDB.associateProfileToProfileFlag()(
+              accountSid,
+              createdProfile.profiles[0].id,
+              defaultFlags[0].id,
+            )
+          ).unwrap();
+
+          const pfs = (
+            await profilesDB.getProfileById()(accountSid, createdProfile.profiles[0].id)
+          ).profileFlags;
+          if (!pfs.includes(defaultFlags[0].id)) {
+            throw new Error('Missing expected association');
+          }
+        });
+
+        afterAll(async () => {
+          // Dissasociate
+          await db.task(t =>
+            t.none(
+              `DELETE FROM "ProfilesToProfileFlags" WHERE "profileId" = ${createdProfile.profiles[0].id}`,
+            ),
+          );
+        });
+
+        each([
+          {
+            description: 'auth is missing',
+            expectStatus: 401,
+            customHeaders: {},
+          },
+          {
+            description: 'profile does not exists',
+            profileId: 0,
+            expectStatus: 404,
+          },
+          {
+            description: 'flag does not exists (no-op)',
+            profileFlagId: 0,
+            expectStatus: 200,
+          },
+          {
+            description: 'profile and flag exist',
+            expectStatus: 200,
+            expectFunction: (response, profileId, profileFlagId) => {
+              expect(response.body.id).toBe(profileId);
+              expect(response.body.profileFlags).not.toContain(profileFlagId);
+            },
+          },
+        ]).test(
+          'when $description, returns $expectStatus',
+          async ({
+            profileId = createdProfile.profiles[0].id,
+            profileFlagId = defaultFlags[0].id,
+            expectStatus,
+            customHeaders,
+            expectFunction,
+          }) => {
+            const response = await request
+              .delete(buildRoute(profileId, profileFlagId))
+              .set(customHeaders || headers);
+            expect(response.statusCode).toBe(expectStatus);
+            console.log(response.body);
+            if (expectFunction) {
+              expectFunction(response, profileId, profileFlagId);
+            }
+          },
         );
       });
     });
   });
 
-  describe('/profileFlags', () => {
-    const defaultFlags = ['abusive', 'blocked'];
-
+  describe('/flags', () => {
     describe('GET', () => {
-      const route = `${baseRoute}/profileFlags`;
+      const defaultFlags = ['abusive', 'blocked'];
+
+      const route = `${baseRoute}/flags`;
 
       test('when no custom flags are added, return default flags', async () => {
         const response = await request.get(route).set(headers);
+
         expect(response.statusCode).toBe(200);
         // We know that there are two "default" flags that every account has: "abusive" and "blocked"
         expect(response.body).toHaveLength(defaultFlags.length);
@@ -356,6 +513,7 @@ describe('/profiles route', () => {
         ).unwrap();
 
         const response = await request.get(route).set(headers);
+
         expect(response.statusCode).toBe(200);
         // We know that there are two "default" flags that every account has: "abusive" and "blocked"
         expect(response.body).toHaveLength(defaultFlags.length + 1);
