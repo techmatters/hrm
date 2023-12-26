100052067f9ceb38fa6ddf6c65ada314a197004a
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2023-12-26 14:30:34 -0300

    DUgh

--- hrm-domain/hrm-service/src/case/caseService.ts
+++ hrm-domain/hrm-service/src/case/caseService.ts
@@ -35,7 +35,7 @@ import {
   update,
 } from './case-data-access';
 import { randomUUID } from 'crypto';
-import type { Contact } from '../contact/contact-data-access';
+import type { Contact } from '../contact/contactDataAccess';
 import { InitializedCan } from '../permissions/initializeCanForRules';
 import type { TwilioUser } from '@tech-matters/twilio-worker-auth';
 import { bindApplyTransformations as bindApplyContactTransformations } from '../contact/contactService';
