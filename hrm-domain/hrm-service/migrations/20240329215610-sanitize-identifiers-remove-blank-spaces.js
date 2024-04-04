f323ee3e6b64b109a31b3f135d724b3285ab38ae
Gianfranco <paolonigianfranco@gmail.com>
2024-04-04 15:36:05 -0300

    fix: Standardize phone number identifiers [CHI-2623] (#613)
    * refactor: sanitize identifiers before get/save
    
    * chore: added migration to remove blank spaces from existing identifiers in the DB

---
+++ hrm-domain/hrm-service/migrations/20240329215610-sanitize-identifiers-remove-blank-spaces.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async queryInterface => {
+    await queryInterface.sequelize.query(`
+    DO $$
+      DECLARE idx public."Identifiers";
+    BEGIN
+      -- For each identifier in Identifier with spaces
+      FOR idx IN (SELECT * FROM "Identifiers" WHERE "identifier" LIKE '% %') LOOP
+        -- Remove any blank spaces that the identifiers might have
+        UPDATE "Identifiers" SET "identifier" = replace(idx."identifier", ' ', '') WHERE id = idx.id;
+      END LOOP;
+    END$$
+    `);
+    console.log('Sequence "Profiles_id_seq" created');
+  },
+
+  down: async () => {},
+};
