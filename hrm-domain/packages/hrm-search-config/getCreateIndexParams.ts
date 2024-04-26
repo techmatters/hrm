673e9d9235cfb02fc943025c8d084b0d2a9c8263
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2024-04-26 13:01:03 -0300

    feat: added config for hrm-search-config package

---
+++ hrm-domain/packages/hrm-search-config/getCreateIndexParams.ts
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+/**
+ * This is a very early example of a rudimentary configuration for a multi-language index in ES.
+ *
+ * There is a lot of room for improvement here to allow more robust use of the ES query string
+ * syntax, but this is a start that gets us close to the functionality we scoped out for cloudsearch.
+ *
+ * see: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html
+ */
+
+import type { IndicesCreateRequest } from '@elastic/elasticsearch/lib/api/types';
+
+import {
+  mappingCasesContacts,
+  HRM_CASES_CONTACTS_INDEX_TYPE,
+} from './hrmIndexDocumentMappings';
+
+const getCreateHrmCasesContactsIndexParams = (index: string): IndicesCreateRequest => {
+  return {
+    index,
+    // settings: {
+    // },
+    mappings: {
+      properties: {
+        high_boost_global: {
+          type: 'text',
+        },
+        low_boost_global: {
+          type: 'text',
+        },
+        ...mappingCasesContacts,
+      },
+    },
+  };
+};
+
+/**
+ * This function is used to make a request to create the resources search index in ES.
+ * @param index
+ */
+export const getCreateIndexParams = (index: string): IndicesCreateRequest => {
+  if (index.endsWith(HRM_CASES_CONTACTS_INDEX_TYPE)) {
+    return getCreateHrmCasesContactsIndexParams(index);
+  }
+
+  throw new Error(`getCreateIndexParams not implemented for index ${index}`);
+};
---
+++ hrm-domain/packages/hrm-search-config/hrmIndexDocumentMappings/mappingCasesContacts.ts
@@ -0,0 +1,119 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+import type { MappingProperty } from '@elastic/elasticsearch/lib/api/types';
+
+// Properties present in root and nested documents
+const commonProperties = {
+  accountSid: {
+    type: 'keyword',
+  },
+  createdBy: {
+    type: 'keyword',
+  },
+  updatedBy: {
+    type: 'keyword',
+  },
+  createdAt: {
+    type: 'date',
+  },
+  updatedAt: {
+    type: 'date',
+  },
+  content: {
+    type: 'text',
+  },
+} as const;
+
+// Properties shared by both types of documents, cases and contacts
+const rootProperties = {
+  type: {
+    type: 'keyword',
+  },
+  id: {
+    type: 'integer',
+  },
+  compundId: {
+    type: 'keyword',
+  },
+  twilioWorkerId: {
+    type: 'keyword',
+  },
+  helpline: {
+    type: 'keyword',
+  },
+  ...commonProperties,
+  join_field: {
+    type: 'join',
+    relations: {
+      case: 'contact',
+    },
+  },
+} as const;
+
+// Properties specific to contacts
+export const contactMapping = {
+  ...rootProperties,
+  number: {
+    type: 'keyword',
+  },
+  channel: {
+    type: 'keyword',
+  },
+  finalizedAt: {
+    type: 'boolean',
+  },
+  timeOfContact: {
+    type: 'date',
+  },
+  transcript: {
+    type: 'text',
+  },
+} as const;
+
+// Properties specific to cases
+export const caseMapping = {
+  ...rootProperties,
+  status: {
+    type: 'keyword',
+  },
+  statusUpdatedAt: {
+    type: 'date',
+  },
+  statusUpdatedBy: {
+    type: 'keyword',
+  },
+  previousStatus: {
+    type: 'keyword',
+  },
+  sections: {
+    type: 'nested',
+    properties: {
+      sectionType: {
+        type: 'keyword',
+      },
+      sectionId: {
+        type: 'keyword',
+      },
+      ...commonProperties,
+    },
+  },
+} as const;
+
+export const mapping: Record<string, MappingProperty> = {
+  ...contactMapping,
+  ...caseMapping,
+} as const;
