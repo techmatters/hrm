9ea15b8e763fe48baf9d2c7db448b74416d03ef2
mythilytm <mythily@techmatters.org>
2024-05-31 17:44:38 -0400

    revert enableProcessContactJobsFlag value

--- hrm-domain/hrm-core/featureFlags.ts
+++ hrm-domain/hrm-core/featureFlags.ts
@@ -17,7 +17,7 @@
 export const enableCreateContactJobsFlag = /^true$/i.test(
   process.env.ENABLE_CREATE_CONTACT_JOBS,
 );
-export const enableProcessContactJobsFlag = /^false$/i.test(
+export const enableProcessContactJobsFlag = /^true$/i.test(
   process.env.ENABLE_PROCESS_CONTACT_JOBS,
 );
 export const enableCleanupJobs = /^true$/i.test(process.env.ENABLE_CLEANUP_JOBS);
