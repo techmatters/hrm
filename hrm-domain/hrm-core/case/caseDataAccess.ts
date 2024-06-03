662f28ce83236b1fa0d5424d1acd39efc60b137d
mythily <102122005+mythilytm@users.noreply.github.com>
2024-06-03 16:32:20 -0400

    CHI-2700 Endpoint for use in generalized search for cases (#643)
    * fix: add createdBy to external recordings
    
    * feat: implement business and data access logic for generalized search on contacts
    
    * fix: change the endpoint to GET
    
    * fix: revert the endpoint to POST
    
    * fix: remove references to search filters, cleanup
    
    * fix: ensure return type is string for getContactsByIds
    
    * case search endpoint for generalied search
    
    * feat: case endpoint in progress sql
    
    * address sql error for cases
    
    * fix: cases sql
    
    * add permissions logic for the sql function
    
    * revert enableProcessContactJobsFlag value
    
    * chore: fix wrong types, removed redudant where clause
    
    ---------
    
    Co-authored-by: Gianfranco Paoloni <paolonigianfranco@gmail.com>

--- hrm-domain/hrm-core/case/caseDataAccess.ts
+++ hrm-domain/hrm-core/case/caseDataAccess.ts
@@ -20,6 +20,7 @@ import { PATCH_CASE_INFO_BY_ID, updateByIdSql } from './sql/caseUpdateSql';
 import {
   OrderByColumnType,
   SearchQueryBuilder,
+  selectCasesByIds,
   selectCaseSearch,
   selectCaseSearchByProfileId,
 } from './sql/caseSearchSql';
@@ -297,3 +298,15 @@ export const updateCaseInfo = async (
     });
   });
 };
+
+export const searchByCaseIds = generalizedSearchQueryFunction<{
+  caseIds: CaseRecord['id'][];
+}>(selectCasesByIds, (accountSid, user, searchCriteria, filters, limit, offset) => {
+  return {
+    accountSid,
+    limit,
+    offset,
+    caseIds: searchCriteria.caseIds,
+    twilioWorkerSid: user.workerSid,
+  };
+});
