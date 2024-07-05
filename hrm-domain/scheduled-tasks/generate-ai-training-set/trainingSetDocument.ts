6aed1ed821ff69662132db18aeb57c30383a5f0b
Stephen Hand <steveh@techmatters.org>
2024-07-05 10:53:13 +0100

    Fix query & build

--- hrm-domain/scheduled-tasks/generate-ai-training-set/trainingSetDocument.ts
+++ hrm-domain/scheduled-tasks/generate-ai-training-set/trainingSetDocument.ts
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see https://www.gnu.org/licenses/.
  */
+
 import ReadableStream = NodeJS.ReadableStream;
 import { TrainingSetContact } from './hrmdbAccess';
 import { ExportTranscript } from '@tech-matters/hrm-types/ConversationMedia';
