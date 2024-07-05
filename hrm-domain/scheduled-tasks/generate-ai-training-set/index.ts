df805e4b413a79d39dac63809db6c13ea9954b0f
Stephen Hand <steveh@techmatters.org>
2024-07-05 17:53:28 +0100

    Comments

--- hrm-domain/scheduled-tasks/generate-ai-training-set/index.ts
+++ hrm-domain/scheduled-tasks/generate-ai-training-set/index.ts
@@ -44,6 +44,17 @@ const lookupAccountSids = async (
 /**
  * This function will pull the contacts from the database, look up their transcript in s3 and combine them to generate a training set for the AI model
  * It saves the output twice, once as file per contact, once as a single file for each helpline
+ * Instead of paginating batches  (which are inefficient in postgres) this uses a streaming approach to ensure that the entire dataset is not loaded into memory
+ * The QueryStream object is used to produce a stream of record objects from the database.
+ * The Transform object is used to take each record object, attach the transcript to it, serialize it to JSON and upload it to S3
+ * The stream of JSON is them passed to an Upload helper object provided by AWS, which handles piping it out to an s3 object
+ * The HIGH_WATER_MARK determines how many records can be pushed into memory before waiting for more records to be pushed all the way through the stream pipeline to s3.
+ * Just remember that calling a streaming operation like 'pipe' doesn't complete that part of the pipeline for all data, it just sets it up, only when the terminating operation is awaited does the pipeline run, and only when that promise resolves will all the steps of the pipeline be done.
+ * @param environment - the environment to run in
+ * @param hlShortCodes - the shortcodes of the helplines to generate training sets for
+ * @param targetBucket - the bucket to upload the training sets to
+ * @param sourceBucket - the bucket to read the transcripts from - this can be used to point it at a different bucket from the one in the database, if this is where scrubbed transcripts have been copied, for example
+ * If sourceBucket is set, the transcript key will be prefixed with the helpline short code as it is assumed that the transcripts for all helplines are in the same bucket.
  */
 export const generate = async (
   environment: 'development' | 'staging' | 'production',
