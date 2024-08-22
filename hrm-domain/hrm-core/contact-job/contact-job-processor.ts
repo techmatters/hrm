76a6d7bc9463f297805f69b96e6aebf8c8dca28d
Stephen Hand <steveh@techmatters.org>
2024-08-22 11:34:38 +0100

    Allow a longer retry period for transcript scrubber

--- hrm-domain/hrm-core/contact-job/contact-job-processor.ts
+++ hrm-domain/hrm-core/contact-job/contact-job-processor.ts
@@ -15,16 +15,27 @@
  */
 
 import { setInterval } from 'timers';
-import { subMilliseconds } from 'date-fns';
-import { pullDueContactJobs } from './contact-job-data-access';
+import { isAfter, parseISO, subMilliseconds } from 'date-fns';
+import {
+  ContactJob,
+  markJobsAsAttempted,
+  pullDueContactJobs,
+} from './contact-job-data-access';
 import { pollAndProcessCompletedContactJobs } from './contact-job-complete';
 import { ContactJobPollerError } from './contact-job-error';
 import { publishDueContactJobs } from './contact-job-publish';
+import { ContactJobType } from '@tech-matters/types/dist';
+import { db } from '../connection-pool';
 
 let processingJobs = false;
 
 const JOB_PROCESSING_INTERVAL_MILLISECONDS = 5000; // 5 seconds
-const JOB_RETRY_INTERVAL_MILLISECONDS = 120000; // 2 minutes
+const MINIMUM_JOB_RETRY_INTERVAL_MILLISECONDS = 120000; // 2 minutes
+const JOB_TYPE_SPECIFIC_RETRY_INTERVAL_MILLISECONDS: {
+  [jobType in ContactJobType]?: number | undefined;
+} = {
+  [ContactJobType.SCRUB_CONTACT_TRANSCRIPT]: 30 * 60 * 1000, // 30 minutes
+};
 export const JOB_MAX_ATTEMPTS = 20;
 
 export function processContactJobs() {
@@ -37,12 +48,25 @@ export function processContactJobs() {
     return setInterval(async () => {
       try {
         await pollAndProcessCompletedContactJobs(JOB_MAX_ATTEMPTS);
-
-        const dueContactJobs = await pullDueContactJobs(
-          subMilliseconds(new Date(), JOB_RETRY_INTERVAL_MILLISECONDS),
-          JOB_MAX_ATTEMPTS,
-        );
-
+        const now = new Date();
+        let dueContactJobs: ContactJob[] = [];
+        await db.tx(async t => {
+          const candidateDueContactJobs = await pullDueContactJobs(
+            t,
+            subMilliseconds(now, MINIMUM_JOB_RETRY_INTERVAL_MILLISECONDS),
+            JOB_MAX_ATTEMPTS,
+          );
+          dueContactJobs = candidateDueContactJobs.filter(job => {
+            const millis = JOB_TYPE_SPECIFIC_RETRY_INTERVAL_MILLISECONDS[job.jobType];
+            return (
+              !millis || isAfter(subMilliseconds(now, millis), parseISO(job.lastAttempt))
+            );
+          });
+          await markJobsAsAttempted(
+            t,
+            dueContactJobs.map(job => job.id),
+          );
+        });
         await publishDueContactJobs(dueContactJobs);
       } catch (err) {
         console.error(
