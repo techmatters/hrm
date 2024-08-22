0dccffe65544bdb8b59c56e58365f108f3d7be00
Stephen Hand <steveh@techmatters.org>
2024-08-22 21:30:08 +0100

    Fix unit test after merge

--- hrm-domain/hrm-core/unit-tests/contact-job/contact-job-complete.test.ts
+++ hrm-domain/hrm-core/unit-tests/contact-job/contact-job-complete.test.ts
@@ -297,19 +297,6 @@ describe('pollAndProcessCompletedContactJobs', () => {
       expect(deletedCompletedContactJobsSpy).toHaveBeenCalledWith(
         validPayload.ReceiptHandle,
       );
-      if (job.jobType === ContactJobType.SCRUB_CONTACT_TRANSCRIPT) {
-        const completedJob = {
-          jobType: job.jobType,
-          jobId: job.jobId,
-          accountSid: job.accountSid,
-          contactId: job.contactId,
-          taskId: job.taskId,
-          twilioWorkerId: job.twilioWorkerId,
-          attemptNumber: job.attemptNumber,
-          originalLocation: job.originalLocation,
-        };
-        expect(publishToContactJobsTopicSpy).toHaveBeenCalledWith(completedJob);
-      }
 
       expect(result?.[0].status).toBe('fulfilled');
     },
