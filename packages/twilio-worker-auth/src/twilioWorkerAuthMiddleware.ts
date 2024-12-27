072ad63f4e1f16d7994be4d08759470cf63a6c3d
Stephen Hand <steveh@techmatters.org>
2024-12-27 08:34:01 +0000

    Remove static key auth from public contact creation endpoint

--- packages/twilio-worker-auth/src/twilioWorkerAuthMiddleware.ts
+++ packages/twilio-worker-auth/src/twilioWorkerAuthMiddleware.ts
@@ -46,12 +46,6 @@ declare global {
 const canAccessResourceWithStaticKey = (path: string, method: string): boolean => {
   // If the requests is to create a new post survey record, grant access
   if (path.endsWith('/postSurveys') && method === 'POST') return true;
-  if (
-    (process.env.TASK_ROUTER_CONTACT_CREATION || '').toLowerCase() === 'true' &&
-    path.endsWith('/contacts') &&
-    method === 'POST'
-  )
-    return true;
 
   // If the requests is retrieve the list of flags associated to a given identifier, grant access
   if (/\/profiles\/identifier\/[^/]+\/flags$/.test(path) && method === 'GET') return true;
