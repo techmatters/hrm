afe9d160565e1ad9a3c5f0e8fac4872cbc61c8d7
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2025-02-24 18:25:09 -0300

    chore: add auth to dispatcher lambda

--- hrm-domain/lambdas/custom-integrations/uscr-dispatcher/index.ts
+++ hrm-domain/lambdas/custom-integrations/uscr-dispatcher/index.ts
@@ -16,15 +16,96 @@
 
 // eslint-disable-next-line prettier/prettier
 import type { ALBEvent, ALBResult } from 'aws-lambda';
-// import { twilioTokenValidator } from '@tech-matters/twilio-worker-auth';
+import { twilioTokenValidator } from '@tech-matters/twilio-worker-auth';
+import { isErr, newErr } from '@tech-matters/types';
+import { getSsmParameter } from '@tech-matters/ssm-cache';
+
+const validAccountSidsMap: { [env: string]: string[] } = {
+  development: ['/development/twilio/AS/account_sid'],
+  staging: ['/staging/twilio/AS/account_sid', '/staging/twilio/USCR/account_sid'],
+};
+
+const authenticateRequest = async ({
+  accountSid,
+  authHeader,
+  environment,
+}: {
+  accountSid: string;
+  authHeader: string;
+  environment: string;
+}) => {
+  const validAccountSids = await Promise.all(
+    validAccountSidsMap[environment].map(key => getSsmParameter(key)),
+  );
+  if (!validAccountSids.includes(accountSid)) {
+    return newErr({
+      error: 'Token authentication failed',
+      message: `Account ${accountSid} not allowed to call this service`,
+    });
+  }
+
+  const authToken = await getSsmParameter(
+    `/${process.env.NODE_ENV}/twilio/${accountSid}/auth_token`,
+  );
+
+  const [type, token] = authHeader.split(' ');
+  if (type !== 'Bearer ') {
+    return newErr({
+      error: 'Token authentication failed',
+      message: `Invalid auth type ${type}`,
+    });
+  }
+
+  const result = await twilioTokenValidator({ accountSid, authToken, token });
+  return result;
+};
 
 export const handler = async (event: ALBEvent): Promise<ALBResult> => {
   try {
     console.log(JSON.stringify(event));
-    // const payload = JSON.parse(event.body || '{}');
+    const payload = JSON.parse(event.body || '{}');
+    const { headers } = event;
+
+    const environment = process.env.NODE_ENV;
+    if (!environment) {
+      console.error(
+        `custom-integrations/uscr-dispatcher: error: missing environment env var`,
+      );
+      return { statusCode: 500 };
+    }
+
+    const accountSid = payload.accountSid;
+    if (!accountSid) {
+      console.warn(
+        `custom-integrations/uscr-dispatcher: error: missing accountSid in request payload`,
+      );
+      return { statusCode: 400 };
+    }
+
+    const authHeader = headers?.authorization;
+    if (!authHeader) {
+      console.warn(
+        `custom-integrations/uscr-dispatcher: error: missing authorization in request headers`,
+      );
+      return { statusCode: 400 };
+    }
+
+    const authResult = await authenticateRequest({
+      accountSid,
+      authHeader,
+      environment,
+    });
+
+    if (isErr(authResult)) {
+      console.warn(authResult.error, authResult.message);
+      return { statusCode: 401 };
+    }
+
     console.debug(`custom-integrations/uscr-dispatcher: called with event ${event}`);
   } catch (err) {
     console.error(`custom-integrations/uscr-dispatcher: error: `, err);
+
+    return { statusCode: 500 };
   }
 
   return {
