c65299566ab3c8d1de58d592f90e53b20dbd524a
Stephen Hand <steveh@techmatters.org>
2026-01-16 11:06:27 +0000

    Attempt to move service tests over to localstack instead of sqslite for sqs mocking

--- hrm-domain/hrm-service/service-tests/sqs.ts
+++ hrm-domain/hrm-service/service-tests/sqs.ts
@@ -15,17 +15,12 @@
  */
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-import sqslite from 'sqslite';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { SQS } from 'aws-sdk';
 
 export const setupTestQueues = (queueNames: string[]) => {
-  const sqsService = sqslite({});
   const sqsClient = new SQS({
-    endpoint: `http://localhost:${process.env.LOCAL_SQS_PORT}`,
+    endpoint: process.env.SQS_ENDPOINT,
   });
-  beforeAll(() => sqsService.listen({ port: parseInt(process.env.LOCAL_SQS_PORT!) }));
-  afterAll(() => sqsService.close());
   beforeEach(async () => {
     await Promise.all(
       queueNames.map(async queueName =>
@@ -37,34 +32,8 @@ export const setupTestQueues = (queueNames: string[]) => {
       ),
     );
   });
-  afterEach(async () => {
-    await Promise.allSettled(
-      queueNames.map(async queueName => {
-        try {
-          const resp = await sqsClient
-            .getQueueUrl({
-              QueueName: queueName,
-            })
-            .promise();
-          const testQueueUrl = resp.QueueUrl;
-          await sqsClient
-            .deleteQueue({
-              QueueUrl: testQueueUrl.toString(),
-            })
-            .promise();
-        } catch (err) {
-          console.error(
-            expect.getState().currentTestName,
-            '\nError deleting queue',
-            queueName,
-            err,
-          );
-        }
-      }),
-    );
-  });
+
   return {
-    sqsService,
     sqsClient,
   };
 };
