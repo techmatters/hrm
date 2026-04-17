e9ada5f0963b320213b62f567091db0674dcdb08
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2026-04-17 17:58:18 -0300

    chore: add healthcheck scripts

---
+++ hrm-domain/speech-to-text/proxy-service/commands/pyannote/healthcheck.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2021-2023 Technology Matters
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+export const command = 'healthcheck';
+export const describe = '';
+
+export const builder = {};
+
+export const handler = async ({}) => {
+  console.info('Republishing entities');
+  try {
+    const result = await fetch(new URL(process.env.LOCAL_PYANNOTE_URI!));
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+};
